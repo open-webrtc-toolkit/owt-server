887408d7809084557ce7871bd1fa68fc111cf8ee
Yuqiang Xian <yuqiang.xian@intel.com>
2014-09-23 07:58:19 +0800

    Fix code style issues in BufferManager.
    (Also include some missing pieces of WoogeenTransport.)

--- source/core/mcu/BufferManager.h
+++ source/core/mcu/BufferManager.h
@@ -18,8 +18,8 @@
  * and approved by Intel in writing.
  */
 
-#ifndef BUFFERMANAGER_H_
-#define BUFFERMANAGER_H_
+#ifndef BufferManager_h
+#define BufferManager_h
 #include <boost/lockfree/queue.hpp>
 #include <logger.h>
 
@@ -33,60 +33,59 @@
  */
 
 namespace webrtc {
- class I420VideoFrame;
+class I420VideoFrame;
 }
-
 
 namespace mcu {
 
 #define SLOT_SIZE 16
 
-
 class BufferManager {
-	DECLARE_LOGGER();
+    DECLARE_LOGGER();
+
 public:
-	BufferManager();
-	virtual ~BufferManager();
+    BufferManager();
+    virtual ~BufferManager();
 
-	webrtc::I420VideoFrame* getFreeBuffer();
-	void releaseBuffer(webrtc::I420VideoFrame* frame);
+    webrtc::I420VideoFrame* getFreeBuffer();
+    void releaseBuffer(webrtc::I420VideoFrame*);
 
-	webrtc::I420VideoFrame* getBusyBuffer(int slot);
+    webrtc::I420VideoFrame* getBusyBuffer(int slot);
 
-	/**
-	 * return a busy frame to the original busy slot if it is empty
-	 */
-	webrtc::I420VideoFrame* returnBusyBuffer(webrtc::I420VideoFrame* frame, int slot);
-	/**
-	 * post a free frame to the busy queue of the corresponding slot, return the
-	 * original busy frame at the corresponding slot.
-	 */
-	webrtc::I420VideoFrame* postFreeBuffer(webrtc::I420VideoFrame* frame, int slot);
+    /**
+     * return a busy frame to the original busy slot if it is empty
+     */
+    webrtc::I420VideoFrame* returnBusyBuffer(webrtc::I420VideoFrame*, int slot);
+    /**
+     * post a free frame to the busy queue of the corresponding slot, return the
+     * original busy frame at the corresponding slot.
+     */
+    webrtc::I420VideoFrame* postFreeBuffer(webrtc::I420VideoFrame*, int slot);
+
 private:
+    /* only works for 64bit */
+    static uint64_t exchange(volatile uint64_t* ptr, uint64_t value) {
+        asm volatile("lock;"
+                     "xchgq %0, %1"
+                     : "=r"(value), "=m"(*ptr)
+                     : "0"(value), "m"(*ptr)
+                     : "memory");
+        return value;
+    }
 
-	/* only works for 64bit */
-	static uint64_t exchange(volatile uint64_t* ptr, uint64_t value) {
-	    asm volatile("lock;"
-	                 "xchgq %0, %1"
-	                 : "=r"(value), "=m"(*ptr)
-	                 : "0"(value), "m"(*ptr)
-	                 : "memory");
-	    return value;
-	}
+    /* only works for 64bit */
+    static uint64_t cmpexchange(volatile uint64_t* dst, uint64_t xchg, uint64_t compare) {
+        uint64_t value = 1;
+        asm volatile( "lock; cmpxchgq %2,(%1)"
+                      : "=a" (value) : "r" (dst), "r" (xchg), "0" (compare) : "memory" );
+        return value;
+    }
 
-	/* only works for 64bit */
-	static uint64_t cmpexchange(volatile uint64_t* dst, uint64_t xchg, uint64_t compare) {
-		uint64_t value = 1;
-		asm volatile( "lock; cmpxchgq %2,(%1)"
-		                : "=a" (value) : "r" (dst), "r" (xchg), "0" (compare) : "memory" );
-		return value;
-	}
-
-	// frames in freeQ can be used to copy decoded frame data by the decoder thread
-	boost::lockfree::queue<webrtc::I420VideoFrame*, boost::lockfree::capacity<SLOT_SIZE*2>> freeQ_;
-	// frames in the busyQ is ready for composition by the encoder thread
-	volatile webrtc::I420VideoFrame* busyQ_[SLOT_SIZE];
+    // frames in freeQ can be used to copy decoded frame data by the decoder thread
+    boost::lockfree::queue<webrtc::I420VideoFrame*, boost::lockfree::capacity<SLOT_SIZE*2>> freeQ_;
+    // frames in the busyQ is ready for composition by the encoder thread
+    volatile webrtc::I420VideoFrame* busyQ_[SLOT_SIZE];
 };
 
 } /* namespace mcu */
-#endif /* BUFFERMANAGER_H_ */
+#endif /* BufferManager_h */
