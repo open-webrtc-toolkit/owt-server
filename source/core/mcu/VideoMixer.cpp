29734642d10812d070270c2edbffc6e955d272c7
Yuqiang Xian <yuqiang.xian@intel.com>
2014-09-23 08:36:54 +0800

    Fix code style issues in VideoMixer.

--- source/core/mcu/VideoMixer.cpp
+++ source/core/mcu/VideoMixer.cpp
@@ -33,109 +33,111 @@ using namespace erizo;
 
 namespace mcu {
 
-  DEFINE_LOGGER(VideoMixer, "media.mixers.VideoMixer");
+DEFINE_LOGGER(VideoMixer, "media.mixers.VideoMixer");
 
-  VideoMixer::VideoMixer() {
-     this->setVideoSourceSSRC(55543);
-     this->setAudioSourceSSRC(44444);
-     this->init();
-  }
+VideoMixer::VideoMixer()
+{
+    this->setVideoSourceSSRC(55543);
+    this->setAudioSourceSSRC(44444);
+    this->init();
+}
 
-  VideoMixer::~VideoMixer() {
-	  delete bufferManager_;
-	  delete transport_;
-	  delete subscriber_;
-	  delete op_;
-  }
+VideoMixer::~VideoMixer()
+{
+    delete bufferManager_;
+    delete transport_;
+    delete subscriber_;
+    delete op_;
+}
 
+/**
+ * init could be used for reset the state of this VideoMixer
+ */
+bool VideoMixer::init()
+{
+    bufferManager_ = new BufferManager();
+    subscriber_ = new erizo::OneToManyProcessor();
+    subscriber_->setPublisher(this);
+    transport_ = new WoogeenTransport(subscriber_);
+    op_ = new VCMOutputProcessor();
+    op_->init(transport_, bufferManager_, this);
 
+    return true;
+}
 
-  /**
-   * init could be used for reset the state of this VideoMixer
-   */
-  bool VideoMixer::init() {
+int VideoMixer::deliverAudioData(char* buf, int len, MediaSource* from)
+{
+    //    ELOG_DEBUG("m.videoCodec.bitrate %d\n", m.videoCodec.bitRate);
+    return 0;
+}
 
-	  bufferManager_ = new BufferManager();
-      subscriber_ = new erizo::OneToManyProcessor();
-      subscriber_->setPublisher(this);
-      transport_ = new WoogeenTransport(subscriber_);
-      op_ = new VCMOutputProcessor();
-      op_->init(transport_, bufferManager_, this);
+/**
+ * use vcm to decode/compose/encode the streams, and then deliver to all subscribers
+ * multiple publishers may call to this method simultaneously from different threads.
+ * the incoming buffer is a rtp packet
+ */
+int VideoMixer::deliverVideoData(char* buf, int len, MediaSource* from)
+{
+    ProtectedRTPReceiver* receiver = publishers_[from];
+    if (receiver == NULL)
+        return 0;        // Is this possible when remove publisher is called while media stream still comes in?
 
-      return true;
+    receiver->deliverVideoData(buf, len);
+    return 0;
+}
 
-  }
+/**
+ * Attach a new InputStream to the Transcoder
+ */
+void VideoMixer::addPublisher(MediaSource* puber)
+{
+    int index = assignSlot(puber);
+    ELOG_DEBUG("addPublisher - assigned slot is %d", index);
+    if (publishers_[puber] == NULL) {
+        op_->updateMaxSlot(maxSlot());
+        boost::shared_ptr<VCMInputProcessor> ip(new VCMInputProcessor(index, op_));
+        ip->init(bufferManager_);
+        ProtectedRTPReceiver* protectedRTPReceiver = new ProtectedRTPReceiver(ip);
+        publishers_[puber] = protectedRTPReceiver;
+        // publishers_.insert(std::pair<MediaSource*, woogeen_base::ProtectedRTPReceiver*>(puber, protectedRTPReceiver));
+    } else {
+        assert (!"new publisher added with InputProcessor still available");    // should not go there
+    }
+}
 
-  int VideoMixer::deliverAudioData(char* buf, int len, MediaSource* from) {
-      //	ELOG_DEBUG("m.videoCodec.bitrate %d\n", m.videoCodec.bitRate);
-	  return 0;
-  }
+/**
+ * Attach a new OutputStream to the OneToManyProcessor
+ */
+void VideoMixer::addSubscriber(MediaSink* suber, const std::string& peerId)
+{
+    ELOG_DEBUG("Adding subscriber: videoSinkSSRC is %d", suber->getVideoSinkSSRC());
+    subscriber_->addSubscriber(suber, peerId);
+}
 
-  /**
-   * use vcm to decode/compose/encode the streams, and then deliver to all subscribers
-   * multiple publishers may call to this method simultaneously from different threads.
-   * the incoming buffer is a rtp packet
-   */
-  int VideoMixer::deliverVideoData(char* buf, int len, MediaSource* from) {
-	  ProtectedRTPReceiver* receiver = publishers_[from];
-	  if (receiver == NULL)
-		  return 0;		// Is this possible when remove publisher is called while media stream still comes in?
-	  receiver->deliverVideoData(buf, len);
-	  return 0;
-  }
+void VideoMixer::removeSubscriber(const std::string& peerId)
+{
+    ELOG_DEBUG("removing subscriber: peerId is %s",   peerId.c_str());
+    subscriber_->removeSubscriber(peerId);
+}
 
+void VideoMixer::removePublisher(MediaSource* puber)
+{
+    if (publishers_[puber]) {
+        int index = getSlot(puber);
+        assert(index >= 0);
+        puberSlotMap_[index] = NULL;
+        delete publishers_[puber];
+        publishers_.erase(puber);
+    }
+}
 
-  /**
-   * Attach a new InputStream to the Transcoder
-   */
-  void VideoMixer::addPublisher(MediaSource* puber){
-	  int index = assignSlot(puber);
-	  ELOG_DEBUG("addPublisher - assigned slot is %d", index);
-	  if (publishers_[puber] == NULL) {
-		  op_->updateMaxSlot(maxSlot());
-		  boost::shared_ptr<VCMInputProcessor> ip(new VCMInputProcessor(index, op_));
-		  ip->init(bufferManager_);
-		  ProtectedRTPReceiver* protectedRTPReceiver = new ProtectedRTPReceiver(ip);
-		  publishers_[puber] = protectedRTPReceiver;
-		 // publishers_.insert(std::pair<MediaSource*, woogeen_base::ProtectedRTPReceiver*>(puber, protectedRTPReceiver));
+void VideoMixer::closeSink()
+{ 
+}
 
-	  } else {
-		  assert (!"new publisher added with InputProcessor still available");	// should not go there
-	  }
-  }
-
-  /**
-   * Attach a new OutputStream to the OneToManyProcessor
-   */
-  void VideoMixer::addSubscriber(MediaSink* suber, const std::string& peerId){
-	  ELOG_DEBUG("Adding subscriber: videoSinkSSRC is %d", suber->getVideoSinkSSRC());
-	  subscriber_->addSubscriber(suber, peerId);
-
-  }
-
-  void VideoMixer::removeSubscriber(const std::string& peerId){
-	  ELOG_DEBUG("removing subscriber: peerId is %s",   peerId.c_str());
-	  subscriber_->removeSubscriber(peerId);
-
-  }
-
-  void VideoMixer::removePublisher(MediaSource* puber) {
-	  if (publishers_[puber]) {
-		  int index = getSlot(puber);
-		  assert(index >= 0);
-		  puberSlotMap_[index] = NULL;
-		  delete publishers_[puber];
-		  publishers_.erase(puber);
-	  }
-
-  }
-
-  void VideoMixer::closeSink() {
-	  //    this->closeAll();
-  }
-
-  void VideoMixer::closeAll() {
-  }
+void VideoMixer::closeAll()
+{
+}
 
 }/* namespace mcu */
 
--- source/core/mcu/VideoMixer.h
+++ source/core/mcu/VideoMixer.h
@@ -18,18 +18,16 @@
  * and approved by Intel in writing.
  */
 
-#ifndef VIDEOMIXER_H_
-#define VIDEOMIXER_H_
-
-#include <map>
-#include <vector>
+#ifndef VideoMixer_h
+#define VideoMixer_h
 
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <logger.h>
+#include <map>
 #include <MediaDefinitions.h>
 #include <OneToManyProcessor.h>
-
+#include <vector>
 
 namespace woogeen_base {
 class ProtectedRTPReceiver;
@@ -42,6 +40,7 @@ class WebRtcConnection;
 }
 
 namespace mcu {
+
 class BufferManager;
 class VCMOutputProcessor;
 
@@ -52,94 +51,89 @@ class VCMOutputProcessor;
  * use cases.
  */
 class VideoMixer : public erizo::MediaSink, public erizo::MediaSource {
-	DECLARE_LOGGER();
+    DECLARE_LOGGER();
+
 public:
+    VideoMixer();
+    virtual ~VideoMixer();
+    virtual bool init();
+    /**
+     * Add a Publisher.
+     * Each publisher will be served by a InputProcessor, which is responsible for
+     * decoding the incoming streams into I420Frames
+     * @param webRtcConn The WebRtcConnection of the Publisher
+     */
+    void addPublisher(erizo::MediaSource* puber);
+    /**
+     * Sets the subscriber
+     * @param webRtcConn The WebRtcConnection of the subscriber
+     * @param peerId An unique Id for the subscriber
+     */
+    void addSubscriber(erizo::MediaSink* suber, const std::string& peerId);
+    /**
+     * Eliminates the subscriber
+     * @param puber
+     */
+    void removeSubscriber(const std::string& peerId);
 
-	VideoMixer();
-	virtual ~VideoMixer();
-	virtual bool init();
-	/**
-	 * Add a Publisher.
-	 * Each publisher will be served by a InputProcessor, which is responsible for
-	 * decoding the incoming streams into I420Frames
-	 * @param webRtcConn The WebRtcConnection of the Publisher
-	 */
-	void addPublisher(erizo::MediaSource* puber);
-	/**
-	 * Sets the subscriber
-	 * @param webRtcConn The WebRtcConnection of the subscriber
-	 * @param peerId An unique Id for the subscriber
-	 */
-	void addSubscriber(erizo::MediaSink* suber, const std::string& peerId);
-	/**
-	 * Eliminates the subscriber
-	 * @param puber
-	 */
-	void removeSubscriber(const std::string& peerId);
+    void removePublisher(erizo::MediaSource* puber);
 
-	void removePublisher(erizo::MediaSource* puber);
+    /**
+     * called by WebRtcConnections' onTransportData. This VideoMixer
+     * will be set as the MediaSink of all the WebRtcConnections in the
+     * same room
+     */
+    virtual int deliverAudioData(char* buf, int len, MediaSource* from);
+    /**
+     * called by WebRtcConnections' onTransportData. This VideoMixer
+     * will be set as the MediaSink of all the WebRtcConnections in the
+     * same room
+     */
+    virtual int deliverVideoData(char* buf, int len, MediaSource* from);
 
-	/**
-	 * called by WebRtcConnections' onTransportData. This VideoMixer
-	 * will be set as the MediaSink of all the WebRtcConnections in the
-	 * same room
-	 */
-	virtual int deliverAudioData(char* buf, int len, MediaSource* from);
-	/**
-	 * called by WebRtcConnections' onTransportData. This VideoMixer
-	 * will be set as the MediaSink of all the WebRtcConnections in the
-	 * same room
-	 */
-	virtual int deliverVideoData(char* buf, int len, MediaSource* from);
+    // implement MediaSource
+    virtual int sendFirPacket() { return 0; }
 
-  	// implement MediaSource
-  	virtual int sendFirPacket() { return 0; }
+    void closeSink();
 
-  	void closeSink();
+    /**
+     * Closes all the subscribers and the publisher, the object is useless after this
+     */
+    void closeAll();
 
+    int assignSlot(erizo::MediaSource* pub) {
+        for (int i = 0; i < puberSlotMap_.size(); i++) {
+            if (puberSlotMap_[i] == NULL) {
+                puberSlotMap_[i] = pub;
+                return i;
+            }
+        }
+        puberSlotMap_.push_back(pub);
+        return puberSlotMap_.size() - 1;
+    }
 
-	/**
-	 * Closes all the subscribers and the publisher, the object is useless after this
-	 */
-	void closeAll();
+    int maxSlot() {
+        return puberSlotMap_.size();
+    }
 
-	int assignSlot(erizo::MediaSource* pub) {
-		for (int i = 0; i < puberSlotMap_.size(); i++) {
-			if (puberSlotMap_[i] == NULL) {
-				puberSlotMap_[i] = pub;
-				return i;
-			}
-		}
-		puberSlotMap_.push_back(pub);
-		return puberSlotMap_.size() - 1;
-	}
-
-	int maxSlot() {
-		return puberSlotMap_.size();
-	}
-
-	// find the slot number for the corresponding puber
-	// return -1 if not found
-	int getSlot(erizo::MediaSource* pub) {
-		for (int i = 0; i < puberSlotMap_.size(); i++) {
-			if (puberSlotMap_[i] == pub)
-				return i;
-		}
-		return -1;
-
-	}
+    // find the slot number for the corresponding puber
+    // return -1 if not found
+    int getSlot(erizo::MediaSource* pub) {
+        for (int i = 0; i < puberSlotMap_.size(); i++) {
+            if (puberSlotMap_[i] == pub)
+                return i;
+        }
+        return -1;
+    }
 
 private:
-
-
-	erizo::OneToManyProcessor* subscriber_;
-	std::map<erizo::MediaSource*, woogeen_base::ProtectedRTPReceiver*> publishers_;
-	std::vector<erizo::MediaSource*> puberSlotMap_;	// each publisher will be allocated one index
-	VCMOutputProcessor* op_;
-	woogeen_base::WoogeenTransport* transport_;
-	BufferManager* bufferManager_;
-
+    erizo::OneToManyProcessor* subscriber_;
+    std::map<erizo::MediaSource*, woogeen_base::ProtectedRTPReceiver*> publishers_;
+    std::vector<erizo::MediaSource*> puberSlotMap_;    // each publisher will be allocated one index
+    VCMOutputProcessor* op_;
+    woogeen_base::WoogeenTransport* transport_;
+    BufferManager* bufferManager_;
 };
 
 } /* namespace mcu */
-#endif /* VIDEOMIXER_H_ */
+#endif /* VideoMixer_h */
