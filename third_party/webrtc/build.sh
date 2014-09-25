0188ce695ee0ab2ef0e3f4a41debc463b538bc34
Yuqiang Xian <yuqiang.xian@intel.com>
2014-09-26 03:19:47 +0800

    Revert "build libwebrtc.a for VCM based mixer"
    This reverts commit 9715efe8d427c78d1ac72e06e7729d4e8ed88ea5.

--- third_party/webrtc/build.sh
+++ third_party/webrtc/build.sh
@@ -7,28 +7,29 @@ export CXXFLAGS="-fPIC -O3 -DNDEBUG -Wall"
 cd $CURRENT_DIR
 
 # Generate the Makefiles
-#if hash node-gyp 2>/dev/null && uname -a | grep [Uu]buntu -q -s; then
-#  PATH=$(dirname $(which node-gyp))/../lib/node_modules/node-gyp/gyp:$PATH
-#fi
-gyp --depth=. -D target_arch="x64" -D os_posix=1 -D clang=0 -D build_with_chromium=0 -D include_tests=0 -D enable_protobuf=0 -D use_system_yasm=0 -D build_libvpx=0 webrtc/webrtc.gyp
+if hash node-gyp 2>/dev/null && uname -a | grep [Uu]buntu -q -s; then
+  PATH=$(dirname $(which node-gyp))/../lib/node_modules/node-gyp/gyp:$PATH
+fi
+gyp --depth=. -D target_arch="x64" -D os_posix=1 -D clang=0 -D build_with_chromium=0 webrtc/webrtc.gyp
 
 # Workaround to not generating thin archives
 sed -i 's/crsT/crs/g' Makefile
 make
 
 # Combine the necessary archives to one
-rm libwebrtc.a
 mkdir -p tmp
-find . -name "*.a" -exec cp {} tmp/ \;
+cp out/Default/obj.target/webrtc/modules/libbitrate_controller.a tmp/
+cp out/Default/obj.target/webrtc/modules/librtp_rtcp.a tmp/
+cp out/Default/obj.target/webrtc/modules/libwebrtc_video_coding.a tmp/
+cp out/Default/obj.target/webrtc/modules/video_coding/utility/libvideo_coding_utility.a tmp/
+cp out/Default/obj.target/webrtc/system_wrappers/source/libsystem_wrappers.a tmp/
 cd tmp
-for file in `ls -r ./*.a`
-do
-    ar -x ${file}
-    if [ ! -f ../libwebrtc.a ]
-    then 
-	    ar cq ../libwebrtc.a *.o
-    else
-            ar q ../libwebrtc.a *.o 
-    fi
-    rm *.o
-done
+ar -x libbitrate_controller.a
+ar -x librtp_rtcp.a
+ar -x libvideo_coding_utility.a
+ar -x libwebrtc_video_coding.a
+ar -x libsystem_wrappers.a
+ar crs ../libwebrtc.a *.o
+cd $CURRENT_DIR
+rm -rf tmp
+
