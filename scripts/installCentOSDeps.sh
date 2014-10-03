c72ea0c7dd27e593818b263676f54240eb6952ce
Yuqiang Xian <yuqiang.xian@intel.com>
2014-10-04 00:45:26 +0800

    Build MCU on CentOS.
    Note that python 2.7 is needed by gclient to fetch some third party libraries.

--- scripts/installCentOSDeps.sh
+++ scripts/installCentOSDeps.sh
@@ -102,6 +102,7 @@ installYumDeps(){
   sudo yum groupinstall " Development Tools" "Development Libraries " -y
   sudo yum install zlib-devel pkgconfig git subversion libcurl-devel.x86_64 curl log4cxx-devel -y
   sudo yum install gcc gcc-c++ bzip2 bzip2-devel bzip2-libs python-devel gyp nodejs npm -y
+  sudo yum install rabbitmq-server mongodb mongodb-server java-1.7.0-openjdk -y
   sudo -E npm install -g --loglevel error node-gyp grunt-cli underscore
 
   read -p "Installing glib2... [Yes/no]" yn
