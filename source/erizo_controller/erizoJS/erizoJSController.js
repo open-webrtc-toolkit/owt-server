c6d683c0c16780778cc20c71f5c649415ddfe6c9
dkrax <dk@krax.net>
2014-10-08 12:03:08 +0200

    addressed some issues when sending json objects when strings were expected

--- source/erizo_controller/erizoJS/erizoJSController.js
+++ source/erizo_controller/erizoJS/erizoJSController.js
@@ -62,6 +62,7 @@ exports.ErizoJSController = function (spec) {
      * Given a WebRtcConnection waits for the state CANDIDATES_GATHERED for set remote SDP.
      */
     initWebRtcConnection = function (wrtc, sdp, callback, id_pub, id_sub) {
+        if(typeof sdp != 'string') sdp = JSON.stringify(sdp); // fixes some issues with sending json object as json, and not as string
 
         if (GLOBAL.config.erizoController.sendStats) {
             wrtc.getStats(function (newStats){
@@ -233,6 +234,7 @@ exports.ErizoJSController = function (spec) {
      * OneToManyProcessor.
      */
     that.addSubscriber = function (from, to, audio, video, sdp, callback) {
+        if (typeof sdp != 'string') sdp = JSON.stringify(sdp);
 
         if (publishers[to] !== undefined && subscribers[to].indexOf(from) === -1 && sdp.match('OFFER') !== null) {
 
