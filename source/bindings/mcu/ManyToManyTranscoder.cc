5444e0456127e12a7f09941f827e61929c7e2369
Yuqiang Xian <yuqiang.xian@intel.com>
2014-11-04 06:21:15 +0800

    Clean-up the ManyToManyTranscoder in the binding layer.

--- source/bindings/mcu/ManyToManyTranscoder.cc
+++ source/bindings/mcu/ManyToManyTranscoder.cc
@@ -21,6 +21,7 @@
 #ifndef BUILDING_NODE_EXTENSION
 #define BUILDING_NODE_EXTENSION
 #endif
+
 #include "node.h"
 #include "ManyToManyTranscoder.h"
 #include <MCU.h>
@@ -31,119 +32,106 @@ ManyToManyTranscoder::ManyToManyTranscoder() {};
 ManyToManyTranscoder::~ManyToManyTranscoder() {};
 
 void ManyToManyTranscoder::Init(Handle<Object> target) {
-	  // Prepare constructor template
-	  Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
-	  tpl->SetClassName(String::NewSymbol("ManyToManyTranscoder"));
-	  tpl->InstanceTemplate()->SetInternalFieldCount(1);
-	  // Prototype
-	  tpl->PrototypeTemplate()->Set(String::NewSymbol("close"), FunctionTemplate::New(close)->GetFunction());
-	  tpl->PrototypeTemplate()->Set(String::NewSymbol("addPublisher"), FunctionTemplate::New(addPublisher)->GetFunction());
-	  tpl->PrototypeTemplate()->Set(String::NewSymbol("removePublisher"), FunctionTemplate::New(removePublisher)->GetFunction());
-	  tpl->PrototypeTemplate()->Set(String::NewSymbol("addSubscriber"), FunctionTemplate::New(addSubscriber)->GetFunction());
-	  tpl->PrototypeTemplate()->Set(String::NewSymbol("removeSubscriber"), FunctionTemplate::New(removeSubscriber)->GetFunction());
-	  tpl->PrototypeTemplate()->Set(String::NewSymbol("sendFIR"), FunctionTemplate::New(sendFIR)->GetFunction());
+    // Prepare constructor template
+    Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
+    tpl->SetClassName(String::NewSymbol("ManyToManyTranscoder"));
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    // Prototype
+    tpl->PrototypeTemplate()->Set(String::NewSymbol("close"), FunctionTemplate::New(close)->GetFunction());
+    tpl->PrototypeTemplate()->Set(String::NewSymbol("addPublisher"), FunctionTemplate::New(addPublisher)->GetFunction());
+    tpl->PrototypeTemplate()->Set(String::NewSymbol("removePublisher"), FunctionTemplate::New(removePublisher)->GetFunction());
+    tpl->PrototypeTemplate()->Set(String::NewSymbol("addSubscriber"), FunctionTemplate::New(addSubscriber)->GetFunction());
+    tpl->PrototypeTemplate()->Set(String::NewSymbol("removeSubscriber"), FunctionTemplate::New(removeSubscriber)->GetFunction());
 
-	  Persistent<Function> constructor = Persistent<Function>::New(tpl->GetFunction());
-	  target->Set(String::NewSymbol("ManyToManyTranscoder"), constructor);
-
+    Persistent<Function> constructor = Persistent<Function>::New(tpl->GetFunction());
+    target->Set(String::NewSymbol("ManyToManyTranscoder"), constructor);
 }
 
 Handle<Value> ManyToManyTranscoder::New(const Arguments& args) {
+  HandleScope scope;
 
-	  HandleScope scope;
+  ManyToManyTranscoder* obj = new ManyToManyTranscoder();
+  obj->me = new mcu::MCU();
 
-	  ManyToManyTranscoder* obj = new ManyToManyTranscoder();
-	  obj->me = new mcu::MCU();
+  obj->Wrap(args.This());
 
-	  obj->Wrap(args.This());
-
-	  return args.This();
-
+  return args.This();
 }
 
 Handle<Value> ManyToManyTranscoder::close(const Arguments& args) {
-	HandleScope scope;
+  HandleScope scope;
 
-	ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
-	mcu::MCU *me = obj->me;
-	delete me;
+  ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
+  mcu::MCU *me = obj->me;
+  delete me;
 
-	return scope.Close(Null());
-
+  return scope.Close(Null());
 }
 
 Handle<Value> ManyToManyTranscoder::addPublisher(
-		const Arguments& args) {
-	HandleScope scope;
-	ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
-	mcu::MCU *me = (mcu::MCU*)obj->me;
+    const Arguments& args) {
+  HandleScope scope;
+  ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
+  mcu::MCU *me = (mcu::MCU*)obj->me;
 
-	WebRtcConnection* param = ObjectWrap::Unwrap<WebRtcConnection>(args[0]->ToObject());
-	erizo::WebRtcConnection* wr = (erizo::WebRtcConnection*)param->me;
+  WebRtcConnection* param = ObjectWrap::Unwrap<WebRtcConnection>(args[0]->ToObject());
+  erizo::WebRtcConnection* wr = (erizo::WebRtcConnection*)param->me;
 
-	erizo::MediaSource* ms = dynamic_cast<erizo::MediaSource*>(wr);
-	me->addPublisher(ms);
+  erizo::MediaSource* ms = dynamic_cast<erizo::MediaSource*>(wr);
+  me->addPublisher(ms);
 
-	return scope.Close(Null());
+  return scope.Close(Null());
 }
 
 Handle<Value> ManyToManyTranscoder::removePublisher(
-		const Arguments& args) {
-	HandleScope scope;
-	ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
-	mcu::MCU *me = (mcu::MCU*)obj->me;
+    const Arguments& args) {
+  HandleScope scope;
+  ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
+  mcu::MCU *me = (mcu::MCU*)obj->me;
 
-	WebRtcConnection* param = ObjectWrap::Unwrap<WebRtcConnection>(args[0]->ToObject());
-	erizo::WebRtcConnection* wr = (erizo::WebRtcConnection*)param->me;
+  WebRtcConnection* param = ObjectWrap::Unwrap<WebRtcConnection>(args[0]->ToObject());
+  erizo::WebRtcConnection* wr = (erizo::WebRtcConnection*)param->me;
 
-	erizo::MediaSource* ms = dynamic_cast<erizo::MediaSource*>(wr);
-	me->removePublisher(ms);
+  erizo::MediaSource* ms = dynamic_cast<erizo::MediaSource*>(wr);
+  me->removePublisher(ms);
 
-	return scope.Close(Null());
+  return scope.Close(Null());
 }
 
 Handle<Value> ManyToManyTranscoder::addSubscriber(
-		const Arguments& args) {
-	HandleScope scope;
-	ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
-	mcu::MCU *me = (mcu::MCU*)obj->me;
+    const Arguments& args) {
+  HandleScope scope;
+  ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
+  mcu::MCU *me = (mcu::MCU*)obj->me;
 
-	WebRtcConnection* param = ObjectWrap::Unwrap<WebRtcConnection>(args[0]->ToObject());
-	erizo::WebRtcConnection* wr = (erizo::WebRtcConnection*)param->me;
+  WebRtcConnection* param = ObjectWrap::Unwrap<WebRtcConnection>(args[0]->ToObject());
+  erizo::WebRtcConnection* wr = (erizo::WebRtcConnection*)param->me;
 
-	erizo::MediaSink* ms = dynamic_cast<erizo::MediaSink*>(wr);
+  erizo::MediaSink* ms = dynamic_cast<erizo::MediaSink*>(wr);
 
-	// get the param
-	v8::String::Utf8Value param1(args[1]->ToString());
+  // get the param
+  v8::String::Utf8Value param1(args[1]->ToString());
 
-	// convert it to string
-	std::string peerId = std::string(*param1);
+  // convert it to string
+  std::string peerId = std::string(*param1);
 
-	me->addSubscriber(ms, peerId);
+  me->addSubscriber(ms, peerId);
 
-	return scope.Close(Null());
-
+  return scope.Close(Null());
 }
 
 Handle<Value> ManyToManyTranscoder::removeSubscriber(
-		const Arguments& args) {
-	HandleScope scope;
-	ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
-	mcu::MCU *me = (mcu::MCU*)obj->me;
+    const Arguments& args) {
+  HandleScope scope;
+  ManyToManyTranscoder* obj = ObjectWrap::Unwrap<ManyToManyTranscoder>(args.This());
+  mcu::MCU *me = (mcu::MCU*)obj->me;
 
-	// get the param
-	v8::String::Utf8Value param1(args[0]->ToString());
+  // get the param
+  v8::String::Utf8Value param1(args[0]->ToString());
 
-	// convert it to string
-	std::string peerId = std::string(*param1);
-	me->removeSubscriber(peerId);
+  // convert it to string
+  std::string peerId = std::string(*param1);
+  me->removeSubscriber(peerId);
 
-	return scope.Close(Null());
-
-}
-
-Handle<Value> ManyToManyTranscoder::sendFIR(const Arguments& args) {
-	HandleScope scope;
-	return scope.Close(Null());
-
+  return scope.Close(Null());
 }
