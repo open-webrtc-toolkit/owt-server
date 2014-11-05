4e06a3eb70e1cc3e59022764b60722646021b323
Yuqiang Xian <yuqiang.xian@intel.com>
2014-11-05 14:53:08 -0800

    Remove unused erizo files.

--- source/bindings/erizoAPI/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if hash node-waf 2>/dev/null; then
-  echo 'building with node-waf'
-  rm -rf build
-  node-waf configure build
-else
-  echo 'building with node-gyp'
-  node-gyp rebuild
-fi
