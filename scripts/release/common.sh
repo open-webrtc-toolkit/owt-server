3772462f935e2e1e680317779bd5f5adc8972be9
Markus Chou <yue.y.zhou@intel.com>
2014-12-30 10:55:11 +0800

    update pack: archive content renaming

--- scripts/release/common.sh
+++ scripts/release/common.sh
@@ -8,7 +8,7 @@
 usage() {
   echo
   echo "WooGeen Packaging Script"
-  echo "    This script creates a WooGeen-MCU/Gateway package to dist/"
+  echo "    This script creates a WooGeen-MCU/Gateway package to ${WOOGEEN_DIST}/"
   echo "    You should use scripts/build.sh to build WooGeen-MCU/Gateway first."
   echo
   echo "Usage:"
@@ -32,11 +32,12 @@ encrypt_js() {
 
 archive() {
   local VER="trunk"
+  local DIR=$(dirname ${WOOGEEN_DIST})
   if hash git 2>/dev/null; then
     echo "using git revision as version number."
     VER=$(cd ${ROOT} && git show-ref --head | head -1 | fold -w 8 | head -1)
   fi
   for file in ${WOOGEEN_DIST}/lib/* ; do strip -s "$file" ; done
-  cd ${ROOT} && tar --numeric-owner -czf "Release-${VER}.tgz" ${WOOGEEN_DIST}
-  echo -e "\x1b[32mRelease-${VER}.tgz generated.\x1b[0m"
+  cd ${DIR} && tar --numeric-owner -czf "Release-${VER}.tgz" $(basename ${WOOGEEN_DIST})
+  echo -e "\x1b[32mRelease-${VER}.tgz generated in ${DIR}.\x1b[0m"
 }
