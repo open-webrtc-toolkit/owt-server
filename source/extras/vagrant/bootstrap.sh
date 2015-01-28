095d76f3c95d482e811176ac6228d20d7dc56497
Yuqiang Xian <yuqiang.xian@intel.com>
2015-01-28 09:58:55 +0800

    Correct the vagrant boostrap script.

--- source/extras/vagrant/bootstrap.sh
+++ source/extras/vagrant/bootstrap.sh
@@ -4,9 +4,10 @@ sudo apt-get install -y git
 cd /vagrant
 git clone ssh://git-amr-1.devtools.intel.com:29418/webrtc-woogeen-2.git
 cd webrtc-woogeen-2
-./scripts/installUbuntuDeps.sh --cleanup
+./scripts/installDepsUnattended.sh --cleanup
 ./scripts/build.sh --all
-./scripts/pack.sh --mcu
+./scripts/release/pack.sh --mcu
+./dist/bin/init.sh
 echo "config.erizoController.publicIP = '$1';" >> ./dist/etc/woogeen_config.js
 echo "config.erizo.minport = 30000;" >> ./dist/etc/woogeen_config.js
 echo "config.erizo.maxport = 31000;" >> ./dist/etc/woogeen_config.js
