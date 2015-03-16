57c431e71daeb3f95a648aa8f2ace25c4fedd5aa
Markus Chou <yue.y.zhou@intel.com>
2015-03-16 17:18:15 +0800

    introduce cipher module to do encrypt/decrypt
        use below sample code to generate a encrypted passphrase collection:
    
        cipher.lock(PASSWORD, collection, filename, function cb () {
            console.log('done!');
        });
    
        collection object format: {
            dtls: '',
            nuve: '',
            erizo: '',
            sample: '', // if needed
        }
    
    Change-Id: Iab3cdd0a067f34cc8c50c502606a5da5e24e466a

---
+++ source/common/cipher.js
@@ -0,0 +1,55 @@
+/* global require, module */
+'use strict';
+
+var crypto = require('crypto');
+var algorithm = 'aes-256-ctr';
+var stream = require('stream');
+var fs = require('fs');
+var zlib = require('zlib');
+
+function encrypt (password, text){
+  var cipher = crypto.createCipher(algorithm, password);
+  var enc = cipher.update(text, 'utf8', 'hex');
+  enc += cipher.final('hex');
+  return enc;
+}
+ 
+function decrypt (password, text){
+  var decipher = crypto.createDecipher(algorithm, password);
+  var dec = decipher.update(text, 'hex', 'utf8');
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+function lock (password, object, filename, cb) {
+  var s = new stream.Readable();
+  s._read = function noop() {};
+  s.push(JSON.stringify(object));
+  s.push(null);
+  var out = fs.createWriteStream(filename);
+  out.on('finish', function () {
+    cb();
+  });
+  s.pipe(zlib.createGzip()).pipe(crypto.createCipher(algorithm, password)).pipe(out);
+}
+
+function unlock (password, filename, cb) {
+  var s = fs.createReadStream(filename);
+  var unzip = zlib.createGunzip();
+  var buf = '';
+  unzip.on('data', function (chunk) {
+    buf += chunk.toString();
+  });
+  unzip.on('end', function () {
+    cb(JSON.parse(buf));
+  });
+  s.pipe(crypto.createDecipher(algorithm, password)).pipe(unzip);
+}
+
+module.exports = {
+  encrypt: encrypt,
+  decrypt: decrypt,
+  k: crypto.pbkdf2Sync('woogeen', 'mcu', 100, 128, 'sha256'),
+  lock: lock,
+  unlock: unlock
+};
