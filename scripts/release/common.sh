4d1b467b5ee881cfa9aa4ffb510251dbcc3d3b9e
Yuqiang Xian <yuqiang.xian@intel.com>
2015-03-25 10:19:26 +0800

    Modify the pack script to perform JS encryption only when archiving is needed.
    Otherwise "encrypt" will break the node binary packaging process.
    On the other hand, encryption should be available only when archiving is needed
    because I don't see any benefit to encrypt the JS files in a non-release package.
    
    Change-Id: I0d56f9de0508db46fbe931bddea9ad48daaacfd4

--- scripts/release/common.sh
+++ scripts/release/common.sh
@@ -15,7 +15,7 @@ usage() {
   echo "    --mcu                               set package target to WooGeen-MCU"
   echo "    --gateway                           set package target to WooGeen-Gateway"
   echo "    --archive                           archive & compress to Release-<version>.tgz"
-  echo "    --encrypt                           minify, compress and encrypt js files for release"
+  echo "    --encrypt                           minify, compress and encrypt js files when archiving"
   echo "    --no-node                           put js in package as is"
   echo "    --no-module                         exclude node_modules in package"
   echo "    --help                              print this help"
@@ -31,6 +31,24 @@ encrypt_js() {
 }
 
 archive() {
+  # encryption if needed
+  if ${ENCRYPT} ; then
+    if ! hash uglifyjs 2>/dev/null; then
+      if hash npm 2>/dev/null; then
+        npm install -g --loglevel error uglify-js
+        hash -r
+      else
+        echo >&2 "npm not found."
+        echo >&2 "You need to install node first."
+      fi
+    fi
+    for i in "${ENCRYPT_CAND_PATH[@]}"
+    do
+      find "$i" -type f -name "*.js" | while read line; do
+        encrypt_js "$line"
+      done
+    done
+  fi
   local VER="trunk"
   local DIR=$(dirname ${WOOGEEN_DIST})
   if [[ -z ${PACKAGE_VERSION} ]]; then
