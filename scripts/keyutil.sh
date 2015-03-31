daec9e6a5937b526c96358e0064edce62060c1f1
Markus Chou <yue.y.zhou@intel.com>
2015-03-31 15:31:34 +0800

    update keyutil.sh: create selfsigned certificate in ease
    Change-Id: I8a25a7a91887f4e170b6b6e808e189df867c07c9

--- scripts/keyutil.sh
+++ scripts/keyutil.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-if ! hash openssl 2>/dev/null; then
-    echo >&2 "Error: openssl not found."
-    return 1
+this=$(readlink -f $0)
+this_dir=$(dirname $this)
+OPENSSL_BIN=$(readlink -f ${this_dir}/../build/libdeps/build/bin/openssl)
+OUT_DIR=$(readlink -f ${this_dir}/../cert)
+
+
+if ! hash ${OPENSSL_BIN} 2>/dev/null; then
+    echo >&2 "Error: openssl not found. You should run \`installDeps.sh' or \`installCommonDeps.sh' to install it first."
+    exit 1
 fi
 
-mergePEM() {
-  openssl pkcs12 -export -out certificate.pfx -inkey $1 -in $2
+selfsignPEM() {
+  ${OPENSSL_BIN} req -x509 -newkey rsa:2048 -keyout ${OUT_DIR}/key.pem -out ${OUT_DIR}/cert.pem \
+  -subj "/C=CN/ST=Shanghai/L=Shanghai/O=Intel/OU=WebRTC/CN=webrtc.intel.com" \
+  -days 30
 }
 
-mergePEM $@
+mergePEM() {
+  ${OPENSSL_BIN} pkcs12 -export -out ${OUT_DIR}/certificate.pfx -inkey ${OUT_DIR}/key.pem -in ${OUT_DIR}/cert.pem
+}
+
+selfsignPEM
+mergePEM
