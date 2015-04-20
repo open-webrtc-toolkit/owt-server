66762049bd23580aec4c92f6ae43bcb50b642ea0
Hua, Chunbo <chunbo.hua@intel.com>
2015-04-20 13:58:06 +0800

    Fix script error of hmac-sha implementation

--- source/nuve/nuveClient/tools/compileDebug.sh
+++ source/nuve/nuveClient/tools/compileDebug.sh
@@ -3,7 +3,7 @@
 TARGET=../dist/nuve-debug.js
 
 cat ../lib/xmlhttprequest.js > ${TARGET}
-cat ../src/hmac-sha1.js >> ${TARGET}
+cat ../src/hmac-sha256.js >> ${TARGET}
 cat ../src/N.js >> ${TARGET}
 cat ../src/N.Base64.js >> ${TARGET}
 cat ../src/N.API.js >> ${TARGET}
