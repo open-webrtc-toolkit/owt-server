e8a922a9d6e15673ec881d97ef737adc999efcc1
Wenjing Fang <wenjing.fang@intel.com>
2015-07-27 13:46:29 +0800

    Add 'doc' folder for generating sdk documents.
    	Update the script in gendoc.sh.
    	More description is in 'doc/README'.
    
    Change-Id: I6b31120d3bd63e04a8dba978c617e3fe3514bb36

---
+++ doc/gendoc.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#Requirement: install jsdoc and docstrap, and pass dir paths of woogeensdk and woogeen2.
+#TODO: install script
+#Example usage: ./gendoc.sh /home/bean/workspace/webrtc-woogeensdk/webrtc/src/sdk/peer.js
+
+#DIR SETTINGS　WOOGEEN2
+WOOGEEN2_DIR=`cd ..; pwd`
+echo "======Set WOOGEEN2_DIR:$WOOGEEN2_DIR."
+
+WOOGEEN2DOC_DIR=$WOOGEEN2_DIR/doc
+DOC_DIR_NAME=webrtc-jsdoc-cosmo
+#TEMPLATE SETTINGS
+README_PATH=$WOOGEEN2DOC_DIR/template/README.md
+CONFIG_PATH=$WOOGEEN2DOC_DIR/jsdoc.conf.json
+TEMPLATE_PATH=$WOOGEEN2DOC_DIR/template/
+
+#EXTRA JS TO PACK
+EXTRA_JSPATH=$@
+echo "EXTRA_JS CODE: $EXTRA_JSPATH"
+
+#1. source code to pack in doc
+WG2_CODE="$WOOGEEN2_DIR/source/sdk2/conference/conference.js $WOOGEEN2_DIR/source/sdk2/base/events.js $WOOGEEN2_DIR/source/sdk2/base/stream.js $WOOGEEN2_DIR/source/nuve/nuveClient/src/N.API.js $WOOGEEN2_DIR/source/sdk2/ui/ui.js"
+echo "WOOGEEN2 CODE: $WG2_CODE"
+
+#2. generate doc
+DOC_DIR=$WOOGEEN2DOC_DIR/$DOC_DIR_NAME
+rm -rf $DOC_DIR/
+mkdir -p $DOC_DIR/
+jsdoc --readme $README_PATH -c $CONFIG_PATH -t $TEMPLATE_PATH -d $DOC_DIR $WG2_CODE $EXTRA_JSPATH
+echo "======Genrate Doc: done.In package:"
+echo ">>>>>>$DOC_DIR"
+
+#3. post process
+java JornalProcess $DOC_DIR > postprocess.rec
+rm postprocess.rec
+echo "======Post process: done..."
