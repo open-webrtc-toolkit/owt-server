1cbd7420a28a25c7364e657efff90e04c8e1c6c2
Yuqiang Xian <yuqiang.xian@intel.com>
2015-09-24 16:05:02 +0800

    Change the JobTimer behavior in calculating the next trigger point.
    Previously we calculated the next timer trigger point based on last expiry time,
    which may result in the timer being triggered too frequently if some operation
    takes longer time than the timer interval (e.g., last expiry time + timer interval <
    current time). We now change it to be based on current time (expires_from_now).
    
    Change-Id: I870fc5608c6ca366d2d7527df85ed0d0db3e1375
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/66267
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>

--- source/core/woogeen_base/JobTimer.h
+++ source/core/woogeen_base/JobTimer.h
@@ -76,7 +76,7 @@ private:
     {
         if (!ec) {
             if (!m_isClosing) {
-                m_timer->expires_at(m_timer->expires_at() + boost::posix_time::milliseconds(m_interval));
+                m_timer->expires_from_now(boost::posix_time::milliseconds(m_interval));
                 handleJob();
                 m_timer->async_wait(boost::bind(&JobTimer::onTimeout, this, boost::asio::placeholders::error));
             }
