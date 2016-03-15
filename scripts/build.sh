0c9b164ce8b738e4e76db69f057ca4474d9c462a
Yuqiang Xian <yuqiang.xian@intel.com>
2016-03-15 18:12:18 +0800

    Fix an issue in last commit due to not rebased to latest code.

--- scripts/build.sh
+++ scripts/build.sh
@@ -125,11 +125,11 @@ build_runtime() {
   popd >/dev/null
   # runtime addon
   local NODE_VERSION=
+  ADDON_LIST=$(find ${RUNTIME_ADDON_SRC_DIR} -type f -name "binding.gyp" | xargs dirname)
   [[ ${ADDON_LIST} =~ "oovoo_gateway" ]] &&
     NODE_VERSION=v$(node -e "process.stdout.write(require('${ROOT}/scripts/release/package.gw.json').engine.node)") ||
     NODE_VERSION=v$(node -e "process.stdout.write(require('${ROOT}/scripts/release/package.mcu.json').engine.node)")
   if [[ ${NODE_VERSION} == $(node --version) ]] && hash node-gyp 2>/dev/null; then
-    ADDON_LIST=$(find ${RUNTIME_ADDON_SRC_DIR} -type f -name "binding.gyp" | xargs dirname)
     for ADDON in ${ADDON_LIST}; do
       echo -e "building addon \e[32m$(basename ${ADDON})\e[0m"
       pushd ${ADDON} >/dev/null
