d1955dd2ed24934cc059d13db17a9b29563de7f6
Markus Chou <yue.y.zhou@intel.com>
2016-04-20 15:41:43 +0800

    gateway: remove liboovoo_gateway.so,liberizo.so
    Change-Id: I85027bbc4ec15b9e8213b10c6a2f8e53a2c2fdc6
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/90773
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>

--- scripts/release/pack.sh
+++ scripts/release/pack.sh
@@ -69,5 +69,5 @@ pack_scripts
 ${PACK_MODULE} && install_module
 pack_node
 type pack_libs >/dev/null 2>&1 && pack_libs
-pack_license
+type pack_license >/dev/null 2>&1 && pack_license
 ${PACK_ARCH} && archive
