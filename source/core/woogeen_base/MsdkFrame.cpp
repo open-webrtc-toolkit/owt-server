71277395b1ba1742b872c6af701c1be4ad4e591b
jdai12 <jianhui.j.dai@intel.com>
2016-06-16 15:53:27 +0800

    Bug fix and frame conversion optimization for vp8 codec
    1. clear MsdkFramePool internal list during free/reAlloc
    2. disable set bgcolor feature and force it black, as msdk vpp does not support well
    3. add sse4 fast_copy to improve NV12/I420 frame conversion performance; vp8 1080p
    6fps->30fps
    
    Signed-off-by: jdai12 <jianhui.j.dai@intel.com>
    Change-Id: If67100efa1b795734a5978a245dbb8a7a2839dfd
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/94403
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>
    Tested-by: Xian, Yuqiang <yuqiang.xian@intel.com>

--- source/core/woogeen_base/MsdkFrame.cpp
+++ source/core/woogeen_base/MsdkFrame.cpp
@@ -20,11 +20,8 @@
 
 #ifdef ENABLE_MSDK
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <assert.h>
-#include <stdio.h>
+#include <malloc.h>
+#include <smmintrin.h>
 
 #include "MsdkBase.h"
 #include "MsdkFrame.h"
@@ -37,6 +34,8 @@ MsdkFrame::MsdkFrame(boost::shared_ptr<mfxFrameAllocator> allocator, mfxFrameInf
     : m_allocator(allocator)
     , m_mainSession(NULL)
     , m_needSync(false)
+    , m_nv12TBuffer(NULL)
+    , m_nv12TBufferSize(0)
 {
     memset(&m_surface, 0, sizeof(mfxFrameSurface1));
 
@@ -46,6 +45,14 @@ MsdkFrame::MsdkFrame(boost::shared_ptr<mfxFrameAllocator> allocator, mfxFrameInf
 
 MsdkFrame::~MsdkFrame()
 {
+    printfFuncEnter;
+
+    if (m_nv12TBuffer) {
+        free(m_nv12TBuffer);
+        m_nv12TBuffer = NULL;
+    }
+
+    printfFuncExit;
 }
 
 void MsdkFrame::sync(void)
@@ -71,7 +78,7 @@ void MsdkFrame::sync(void)
     }
 }
 
-bool MsdkFrame::reSize(int width, int height)
+bool MsdkFrame::reSize(uint32_t width, uint32_t height)
 {
     ELOG_TRACE("reSize %dx%d(%dx%d) -> %dx%d"
             , m_surface.Info.CropW, m_surface.Info.CropH
@@ -88,6 +95,12 @@ bool MsdkFrame::reSize(int width, int height)
 
     m_surface.Info.CropW = width;
     m_surface.Info.CropH = height;
+
+    if (m_nv12TBuffer && m_nv12TBufferSize < width * height * 3 / 2) {
+        free(m_nv12TBuffer);
+        m_nv12TBuffer = NULL;
+        m_nv12TBufferSize = 0;
+    }
 
     return true;
 }
@@ -257,7 +270,7 @@ bool MsdkFrame::convertFrom(webrtc::I420VideoFrame& frame)
 
     // set luminance plane
     for(i = 0; i < h; i++) {
-        memcpy(ptr + i * pitch, pSrcY + i * srcStrideY, w);
+        memcpy_f(ptr + i * pitch, pSrcY + i * srcStrideY, w);
     }
 
     // set chroma planes
@@ -300,6 +313,63 @@ bool MsdkFrame::convertFrom(webrtc::I420VideoFrame& frame)
     return ret;
 }
 
+bool MsdkFrame::nv12ConvertTo(mfxFrameInfo& pInfo, mfxFrameData& pData, webrtc::I420VideoFrame& frame)
+{
+    uint32_t w = getWidth();
+    uint32_t h = getHeight();
+
+    if (!m_nv12TBuffer) {
+        m_nv12TBuffer = (uint8_t *)memalign(16, h * pData.Pitch * 3 / 2);
+        if (!m_nv12TBuffer)
+        {
+            ELOG_ERROR("memalign failed, %p", m_nv12TBuffer);
+
+            return false;
+        }
+        m_nv12TBufferSize = h * pData.Pitch * 3 / 2;
+    }
+
+    //uint32_t dstW, dstH;
+    uint32_t dstStrideY, dstStrideU, dstStrideV;
+    uint8_t *pDstY, *pDstU, *pDstV;
+
+    dstStrideY = frame.stride(webrtc::kYPlane);
+    dstStrideU = frame.stride(webrtc::kUPlane);
+    dstStrideV = frame.stride(webrtc::kVPlane);
+
+    pDstY = frame.buffer(webrtc::kYPlane);
+    pDstU = frame.buffer(webrtc::kUPlane);
+    pDstV = frame.buffer(webrtc::kVPlane);
+
+    memcpy_f(m_nv12TBuffer, pData.Y, h * pData.Pitch);
+    memcpy_f(m_nv12TBuffer + h * pData.Pitch, pData.UV, h * pData.Pitch / 2);
+
+    uint8_t *ptrY;
+    uint8_t *ptrUV;
+
+    ptrY = m_nv12TBuffer;
+    ptrUV = m_nv12TBuffer + h * pData.Pitch;
+
+    uint32_t i, j;
+
+    // set luminance plane
+    for(i = 0; i < h; i++) {
+        memcpy_f(pDstY + i * dstStrideY, ptrY + i * pData.Pitch, w);
+    }
+
+    w /= 2;
+    h /= 2;
+
+    for (i = 0; i < h; i++) {
+        for (j = 0; j < w; j++) {
+            pDstU[i * dstStrideU + j] = ptrUV[i * pData.Pitch + j * 2];
+            pDstV[i * dstStrideV + j] = ptrUV[i * pData.Pitch + j * 2 + 1] ;
+        }
+    }
+
+    return true;
+}
+
 bool MsdkFrame::convertTo(webrtc::I420VideoFrame& frame)
 {
     bool ret = false;
@@ -309,14 +379,6 @@ bool MsdkFrame::convertTo(webrtc::I420VideoFrame& frame)
     mfxFrameSurface1 *pSurface = &m_surface;
     mfxFrameInfo& pInfo = pSurface->Info;
     mfxFrameData& pData = pSurface->Data;
-
-    mfxU16 w, h, pitch;
-    mfxU8 *ptr;
-    mfxU32 i, j;
-
-    //uint32_t dstW, dstH;
-    uint32_t dstStrideY, dstStrideU, dstStrideV;
-    uint8_t *pDstY, *pDstU, *pDstV;
 
     if (MFX_FOURCC_NV12 != pInfo.FourCC) {
         ELOG_ERROR("Format (%c%c%c%c) is not upported!",
@@ -329,35 +391,15 @@ bool MsdkFrame::convertTo(webrtc::I420VideoFrame& frame)
         return false;
     }
 
-    if (pInfo.CropH > 0 && pInfo.CropW > 0) {
-        w = pInfo.CropW;
-        h = pInfo.CropH;
-    }
-    else {
-        w = pInfo.Width;
-        h = pInfo.Height;
-    }
-
     frame.CreateEmptyFrame(
-            w,
-            h,
-            w,
-            w / 2,
-            w / 2
+            getWidth(),
+            getHeight(),
+            getWidth(),
+            getWidth() / 2,
+            getWidth() / 2
             );
 
     //dumpI420VideoFrameInfo(frame);
-
-    //dstW = frame.width();
-    //dstH = frame.height();
-
-    dstStrideY = frame.stride(webrtc::kYPlane);
-    dstStrideU = frame.stride(webrtc::kUPlane);
-    dstStrideV = frame.stride(webrtc::kVPlane);
-
-    pDstY = frame.buffer(webrtc::kYPlane);
-    pDstU = frame.buffer(webrtc::kUPlane);
-    pDstV = frame.buffer(webrtc::kVPlane);
 
     sync();
 
@@ -368,30 +410,10 @@ bool MsdkFrame::convertTo(webrtc::I420VideoFrame& frame)
         return false;
     }
 
-    pitch = pData.Pitch;
-    ptr = pData.Y + pInfo.CropX + pInfo.CropY * pData.Pitch;
-
-    // set luminance plane
-    for(i = 0; i < h; i++) {
-        memcpy(pDstY + i * dstStrideY, ptr + i * pitch,  w);
-    }
-
-    // set chroma planes
     switch (pSurface->Info.FourCC)
     {
         case MFX_FOURCC_NV12:
-            w /= 2;
-            h /= 2;
-            ptr = pData.UV + pInfo.CropX + (pInfo.CropY / 2) * pitch;
-
-            for (i = 0; i < h; i++) {
-                for (j = 0; j < w; j++) {
-                    pDstU[i * dstStrideU + j] = ptr[i * pitch + j * 2];
-                    pDstV[i * dstStrideV + j] = ptr[i * pitch + j * 2 + 1] ;
-                }
-            }
-
-            ret = true;
+            ret = nv12ConvertTo(pInfo, pData, frame);
             break;
 
         default:
@@ -438,6 +460,88 @@ void MsdkFrame::dumpI420VideoFrameInfo(webrtc::I420VideoFrame& frame)
             , srcStrideY, srcStrideU, srcStrideV
             , frame.timestamp()
             );
+}
+
+void *MsdkFrame::memcpy_f(void *dst, void *src, size_t size)
+{
+    bool aligned;
+    size_t remain;
+    size_t i, round;
+    __m128i x0, x1, x2, x3, x4, x5, x6, x7;
+    __m128i *pDst, *pSrc;
+
+    if ( dst == NULL || src == NULL ) {
+        return NULL;
+    }
+
+    aligned = (((size_t) dst) | ((size_t) src)) & 0x0F;
+
+    if ( aligned != 0 ) {
+        ELOG_WARN( "Addr is not 16 aligned, do normal copy instead: %p -> %p", src, dst );
+        return memcpy( dst, src, size );
+    }
+
+    pDst = (__m128i *) dst;
+    pSrc = (__m128i *) src;
+    remain = size & 0x7F;
+    round = size >> 7;
+    _mm_mfence();
+
+    for ( i = 0; i < round; i++ ) {
+        x0 = _mm_stream_load_si128( pSrc + 0 );
+        x1 = _mm_stream_load_si128( pSrc + 1 );
+        x2 = _mm_stream_load_si128( pSrc + 2 );
+        x3 = _mm_stream_load_si128( pSrc + 3 );
+        x4 = _mm_stream_load_si128( pSrc + 4 );
+        x5 = _mm_stream_load_si128( pSrc + 5 );
+        x6 = _mm_stream_load_si128( pSrc + 6 );
+        x7 = _mm_stream_load_si128( pSrc + 7 );
+        /*
+        __asm__ __volatile__( "movntdqa (%1), %0":"=x"(x0):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 16(%1), %0":"=x"(x1):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 32(%1), %0":"=x"(x2):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 48(%1), %0":"=x"(x3):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 64(%1), %0":"=x"(x4):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 80(%1), %0":"=x"(x5):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 96(%1), %0":"=x"(x6):"g"(pSrc) );
+        __asm__ __volatile__( "movntdqa 112(%1), %0":"=x"(x7):"g"(pSrc) );
+        */
+        _mm_store_si128( pDst + 0, x0 );
+        _mm_store_si128( pDst + 1, x1 );
+        _mm_store_si128( pDst + 2, x2 );
+        _mm_store_si128( pDst + 3, x3 );
+        _mm_store_si128( pDst + 4, x4 );
+        _mm_store_si128( pDst + 5, x5 );
+        _mm_store_si128( pDst + 6, x6 );
+        _mm_store_si128( pDst + 7, x7 );
+        pSrc += 8;
+        pDst += 8;
+    }
+
+    if ( remain >= 16 ) {
+        size = remain;
+        remain = size & 0xF;
+        round = size >> 4;
+
+        for ( i = 0; i < round; i++ ) {
+            //__asm__ ( "movntdqa (%1), %0":"=x"(x0):"g"(pSrc) );
+            x0 = _mm_stream_load_si128( pSrc + 0 );
+            pSrc += 1;
+            _mm_store_si128( pDst, x0 );
+            pDst += 1;
+        }
+    }
+
+    if ( remain > 0 ) {
+        char *ps = (char *)(pSrc);
+        char *pd = (char *)(pDst);
+
+        for ( i = 0; i < remain; i++ ) {
+            pd[i] = ps[i];
+        }
+    }
+
+    return dst;
 }
 
 MsdkFramePool::MsdkFramePool(boost::shared_ptr<mfxFrameAllocator> allocator, mfxFrameAllocRequest &request)
@@ -526,6 +630,8 @@ bool MsdkFramePool::freeFrames()
         }
     }
 
+    m_framePool.clear();
+
     sts = m_allocator->Free(m_allocator->pthis, &m_response);
     if (sts != MFX_ERR_NONE)
     {
@@ -535,7 +641,7 @@ bool MsdkFramePool::freeFrames()
     return true;
 }
 
-bool MsdkFramePool::reAllocate(int width, int height)
+bool MsdkFramePool::reAllocate(uint32_t width, uint32_t height)
 {
     ELOG_DEBUG("reAllocate %dx%d -> %dx%d", m_allocatedWidth, m_allocatedHeight, width, height);
 
