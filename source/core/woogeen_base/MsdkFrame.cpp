2bdb7e8791c0c3ded75747515b990e6e53028e15
jdai12 <jianhui.j.dai@intel.com>
2016-06-22 16:51:51 +0800

    Move fastcopy out of MsdkFrame and add relevant comment
    Signed-off-by: jdai12 <jianhui.j.dai@intel.com>
    Change-Id: I062c113f2b1e6ef479f912dbdd6abfc4210f15d4
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/94692
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>
    Tested-by: Xian, Yuqiang <yuqiang.xian@intel.com>

--- source/core/woogeen_base/MsdkFrame.cpp
+++ source/core/woogeen_base/MsdkFrame.cpp
@@ -21,7 +21,8 @@
 #ifdef ENABLE_MSDK
 
 #include <malloc.h>
-#include <smmintrin.h>
+
+#include "FastCopy.h"
 
 #include "MsdkBase.h"
 #include "MsdkFrame.h"
@@ -270,7 +271,7 @@ bool MsdkFrame::convertFrom(webrtc::I420VideoFrame& frame)
 
     // set luminance plane
     for(i = 0; i < h; i++) {
-        memcpy_f(ptr + i * pitch, pSrcY + i * srcStrideY, w);
+        memcpy(ptr + i * pitch, pSrcY + i * srcStrideY, w);
     }
 
     // set chroma planes
@@ -341,8 +342,8 @@ bool MsdkFrame::nv12ConvertTo(mfxFrameInfo& pInfo, mfxFrameData& pData, webrtc::
     pDstU = frame.buffer(webrtc::kUPlane);
     pDstV = frame.buffer(webrtc::kVPlane);
 
-    memcpy_f(m_nv12TBuffer, pData.Y, h * pData.Pitch);
-    memcpy_f(m_nv12TBuffer + h * pData.Pitch, pData.UV, h * pData.Pitch / 2);
+    memcpy_from_uswc_sse4(m_nv12TBuffer, pData.Y, h * pData.Pitch);
+    memcpy_from_uswc_sse4(m_nv12TBuffer + h * pData.Pitch, pData.UV, h * pData.Pitch / 2);
 
     uint8_t *ptrY;
     uint8_t *ptrUV;
@@ -354,7 +355,7 @@ bool MsdkFrame::nv12ConvertTo(mfxFrameInfo& pInfo, mfxFrameData& pData, webrtc::
 
     // set luminance plane
     for(i = 0; i < h; i++) {
-        memcpy_f(pDstY + i * dstStrideY, ptrY + i * pData.Pitch, w);
+        memcpy(pDstY + i * dstStrideY, ptrY + i * pData.Pitch, w);
     }
 
     w /= 2;
@@ -462,87 +463,6 @@ void MsdkFrame::dumpI420VideoFrameInfo(webrtc::I420VideoFrame& frame)
             );
 }
 
-void *MsdkFrame::memcpy_f(void *dst, void *src, size_t size)
-{
-    bool aligned;
-    size_t remain;
-    size_t i, round;
-    __m128i x0, x1, x2, x3, x4, x5, x6, x7;
-    __m128i *pDst, *pSrc;
-
-    if ( dst == NULL || src == NULL ) {
-        return NULL;
-    }
-
-    aligned = (((size_t) dst) | ((size_t) src)) & 0x0F;
-
-    if ( aligned != 0 ) {
-        ELOG_WARN( "Addr is not 16 aligned, do normal copy instead: %p -> %p", src, dst );
-        return memcpy( dst, src, size );
-    }
-
-    pDst = (__m128i *) dst;
-    pSrc = (__m128i *) src;
-    remain = size & 0x7F;
-    round = size >> 7;
-    _mm_mfence();
-
-    for ( i = 0; i < round; i++ ) {
-        x0 = _mm_stream_load_si128( pSrc + 0 );
-        x1 = _mm_stream_load_si128( pSrc + 1 );
-        x2 = _mm_stream_load_si128( pSrc + 2 );
-        x3 = _mm_stream_load_si128( pSrc + 3 );
-        x4 = _mm_stream_load_si128( pSrc + 4 );
-        x5 = _mm_stream_load_si128( pSrc + 5 );
-        x6 = _mm_stream_load_si128( pSrc + 6 );
-        x7 = _mm_stream_load_si128( pSrc + 7 );
-        /*
-        __asm__ __volatile__( "movntdqa (%1), %0":"=x"(x0):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 16(%1), %0":"=x"(x1):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 32(%1), %0":"=x"(x2):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 48(%1), %0":"=x"(x3):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 64(%1), %0":"=x"(x4):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 80(%1), %0":"=x"(x5):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 96(%1), %0":"=x"(x6):"g"(pSrc) );
-        __asm__ __volatile__( "movntdqa 112(%1), %0":"=x"(x7):"g"(pSrc) );
-        */
-        _mm_store_si128( pDst + 0, x0 );
-        _mm_store_si128( pDst + 1, x1 );
-        _mm_store_si128( pDst + 2, x2 );
-        _mm_store_si128( pDst + 3, x3 );
-        _mm_store_si128( pDst + 4, x4 );
-        _mm_store_si128( pDst + 5, x5 );
-        _mm_store_si128( pDst + 6, x6 );
-        _mm_store_si128( pDst + 7, x7 );
-        pSrc += 8;
-        pDst += 8;
-    }
-
-    if ( remain >= 16 ) {
-        size = remain;
-        remain = size & 0xF;
-        round = size >> 4;
-
-        for ( i = 0; i < round; i++ ) {
-            //__asm__ ( "movntdqa (%1), %0":"=x"(x0):"g"(pSrc) );
-            x0 = _mm_stream_load_si128( pSrc + 0 );
-            pSrc += 1;
-            _mm_store_si128( pDst, x0 );
-            pDst += 1;
-        }
-    }
-
-    if ( remain > 0 ) {
-        char *ps = (char *)(pSrc);
-        char *pd = (char *)(pDst);
-
-        for ( i = 0; i < remain; i++ ) {
-            pd[i] = ps[i];
-        }
-    }
-
-    return dst;
-}
 
 MsdkFramePool::MsdkFramePool(boost::shared_ptr<mfxFrameAllocator> allocator, mfxFrameAllocRequest &request)
     : m_allocator(allocator)
