551a92836c67a0616e0113cb6b4317103c4f1b4b
Chen, Li1 <li1.chen@intel.com>
2016-08-02 14:13:43 +0800

    Add RTMP fdkaac lib install scripts and fix RTMP url format
    Change-Id: If624b5766ae677cdbba95d536106d2f63d07db6c
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/97331
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>

--- source/portal/test/portal_test.js
+++ source/portal/test/portal_test.js
@@ -1372,7 +1372,7 @@ describe('portal.subscribe/portal.unsubscribe/portal.mediaOnOff: Participants su
           expect(mockRpcClient.subscribe.getCall(0).args[0]).to.deep.equal('rpcIdOfAccessNode');
           expect(mockRpcClient.subscribe.getCall(0).args[1]).to.equal(subscription_id);
           expect(mockRpcClient.subscribe.getCall(0).args[2]).to.equal('avstream');
-          expect(mockRpcClient.subscribe.getCall(0).args[3]).to.deep.equal({audio: {codecs: ['aac']}, video: {codecs: ['h264'], resolution: 'vga'}, url: 'rtmp://user:pwd@1.1.1.1:9000/url-of-avstream/room_' + path.join(testSession + '-' + subscription_id + '.sdp')});
+          expect(mockRpcClient.subscribe.getCall(0).args[3]).to.deep.equal({audio: {codecs: ['aac']}, video: {codecs: ['h264'], resolution: 'vga'}, url: 'rtmp://user:pwd@1.1.1.1:9000/url-of-avstream'});
           on_connection_status = mockRpcClient.subscribe.getCall(0).args[4];
           expect(on_connection_status).to.be.a('function');
           return on_connection_status({type: 'initializing'});
