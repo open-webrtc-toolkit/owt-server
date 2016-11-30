9532f82ce1c704ac2338e967d71a294790f0a04c
jdai12 <jianhui.j.dai@intel.com>
2016-11-30 15:28:53 +0800

    Revert build to download openh264 lib
    Signed-off-by: jdai12 <jianhui.j.dai@intel.com>
    Change-Id: Ia3eed5aa3c311733b4d8a9ab718d3e287a0b9dc5
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/105339
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>

--- source/agent/video/download_openh264.sh
+++ source/agent/video/download_openh264.sh
@@ -7,18 +7,4 @@ download_openh264(){
   echo "Download libopenh264-1.6.0-linux64.3.so success."
 }
 
-# compile master instead of v1.6 release to support weighted_bipred_idc
-# revert below after v1.7 released
-compile_openh264(){
-  echo "Download OpenH264 Source..."
-  git clone https://github.com/cisco/openh264.git
-  cd openh264
-  git checkout 28db1bce906577c6e70d4b801a96882a33452462
-  echo "Download OpenH264 Source success."
-
-  echo "Compile OpenH264..."
-  make -j4
-  echo "Compile libopenh264.so.3 success."
-}
-
-compile_openh264
+download_openh264
