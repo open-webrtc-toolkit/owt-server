6bafdf1d478cfb7275fc6b4e315f206ac59c7cdd
Chen, Li1 <li1.chen@intel.com>
2017-01-20 10:01:23 +0800

    Update copyright with 2017 in script files.
    Change-Id: Ia2b0108333a49854f79d6d903810eeb559282584
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/108637
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Zhai, Lei <lei.zhai@intel.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2016 Intel Corporation All Rights Reserved.
+# Copyright 2017 Intel Corporation All Rights Reserved.
 #
 # The source code contained or described herein and all documents related to the
 # source code ("Material") are owned by Intel Corporation or its suppliers or
