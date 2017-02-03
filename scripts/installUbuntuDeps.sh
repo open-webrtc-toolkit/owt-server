013f30b7216a4f1a99a553ca7a176590c204afc7
Qiu Jianlin <jianlin.qiu@intel.com>
2017-02-03 20:02:36 +0800

    update Ubunbtu build script to allow building on Ubuntu 16.04
    Change-Id: I1e7c434de896aba8748e7b5d8808fae1cce9fef6
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/109588
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>
    Tested-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -14,8 +14,8 @@ enable_intel_gpu_top() {
 
 install_apt_deps(){
   sudo -E apt-get update
-  sudo -E apt-get install git make gcc g++ libssl-dev libglib2.0-dev pkg-config libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl libboost-test-dev nasm yasm gyp libx11-dev libkrb5-dev nload intel-gpu-tools
-  enable_intel_gpu_top libtool automake
+  sudo -E apt-get install git make gcc g++ libssl-dev libglib2.0-dev pkg-config libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-8-jre curl libboost-test-dev nasm yasm gyp libx11-dev libkrb5-dev nload intel-gpu-tools m4 autoconf libtool automake
+  enable_intel_gpu_top
 }
 
 install_mediadeps_nonfree(){
