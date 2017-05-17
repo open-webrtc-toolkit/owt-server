6dca8da5e96c6c5b5b3f23964861df6b39718642
Jianjun Zhu <jianjun.zhu@intel.com>
2017-05-17 12:21:45 +0800

    Ignore bitrate feedback from receiver.
    BUG=1693
    
    Change-Id: If822f9f6797efee52ae09aeb5f35bad86248d0b8
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/159948
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>

--- source/core/woogeen_base/VideoFramePacketizer.cpp
+++ source/core/woogeen_base/VideoFramePacketizer.cpp
@@ -1,20 +1,20 @@
 /*
- * Copyright 2017 Intel Corporation All Rights Reserved. 
- * 
- * The source code contained or described herein and all documents related to the 
- * source code ("Material") are owned by Intel Corporation or its suppliers or 
- * licensors. Title to the Material remains with Intel Corporation or its suppliers 
- * and licensors. The Material contains trade secrets and proprietary and 
- * confidential information of Intel or its suppliers and licensors. The Material 
- * is protected by worldwide copyright and trade secret laws and treaty provisions. 
- * No part of the Material may be used, copied, reproduced, modified, published, 
- * uploaded, posted, transmitted, distributed, or disclosed in any way without 
+ * Copyright 2017 Intel Corporation All Rights Reserved.
+ *
+ * The source code contained or described herein and all documents related to the
+ * source code ("Material") are owned by Intel Corporation or its suppliers or
+ * licensors. Title to the Material remains with Intel Corporation or its suppliers
+ * and licensors. The Material contains trade secrets and proprietary and
+ * confidential information of Intel or its suppliers and licensors. The Material
+ * is protected by worldwide copyright and trade secret laws and treaty provisions.
+ * No part of the Material may be used, copied, reproduced, modified, published,
+ * uploaded, posted, transmitted, distributed, or disclosed in any way without
  * Intel's prior express written permission.
- * 
- * No license under any patent, copyright, trade secret or other intellectual 
- * property right is granted to or conferred upon you by disclosure or delivery of 
- * the Materials, either expressly, by implication, inducement, estoppel or 
- * otherwise. Any license under such intellectual property rights must be express 
+ *
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or delivery of
+ * the Materials, either expressly, by implication, inducement, estoppel or
+ * otherwise. Any license under such intellectual property rights must be express
  * and approved by Intel in writing.
  */
 
@@ -155,9 +155,8 @@ void VideoFramePacketizer::receiveRtpData(char* buf, int len, erizo::DataType ty
 
 void VideoFramePacketizer::OnNetworkChanged(const uint32_t target_bitrate, const uint8_t fraction_loss, const int64_t rtt)
 {
-    FeedbackMsg feedback = {.type = VIDEO_FEEDBACK, .cmd = SET_BITRATE};
-    feedback.data.kbps = target_bitrate / 1000;
-    deliverFeedbackMsg(feedback);
+    // Receiver's network change detected. But we do not deliver feedback to
+    // sender because sender may adjust sending bitrate for a specific receiver.
 }
 
 void VideoFramePacketizer::onFrame(const Frame& frame)
