74fd5b8da8cbc6f07472d5f7469a85fcd51d31a2
Jianjun Zhu <jianjun.zhu@intel.com>
2017-06-05 13:22:04 +0800

    Patch libnice to enhance connectivity with Firefox.
    Firefox does not include use-candidate flag in STUN request for
    keep-alive. It leads some prflx pairs not being nominated if Firefox
    finishes STUN procedure before MCU.
    
    This change only works when all clients use aggressive nomination. It
    may cause new issue if some clients use regular nomination.
    
    Change-Id: Ieb56583788a6659513759aabb749002f28a592b3
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/163100
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>
    Tested-by: Duan, Xiande <xiande.duan@intel.com>
    
    Conflicts:
    	scripts/installCommonDeps.sh
    Change-Id: I591896a26dd4928d0fec860c384733ff2fdca08f
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/178264
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>

--- scripts/installCommonDeps.sh
+++ scripts/installCommonDeps.sh
@@ -83,6 +83,7 @@ install_libnice(){
     wget -c http://nice.freedesktop.org/releases/libnice-0.1.13.tar.gz
     tar -zxvf libnice-0.1.13.tar.gz
     cd libnice-0.1.13
+    patch -p1 < $PATHNAME/libnice-0113.patch
     #patch -p1 < $PATHNAME/patches/libnice-0001-Remove-lock.patch
     PKG_CONFIG_PATH=$PREFIX_DIR"/lib/pkgconfig":$PREFIX_DIR"/lib64/pkgconfig":$PKG_CONFIG_PATH ./configure --prefix=$PREFIX_DIR && make -s V= && make install
     cd $CURRENT_DIR
