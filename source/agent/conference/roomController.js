a9e8ddf6d83bde10c166d3e2a55584869a37e7d6
Xiande Duan <xiande.duan@intel.com>
2017-09-14 14:31:42 +0800

    BUGFIX[363] Starting more than 64 recorders would fail.
    Tracked-On: https://jira01.devtools.intel.com/browse/ICFWX-363
    Change-Id: Ibea181b9968c298bf0e75b8e2564b857bf171967
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/175970
    Reviewed-by: webrtctest <webrtctest@intel.com>
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>

--- source/agent/conference/roomController.js
+++ source/agent/conference/roomController.js
@@ -1236,7 +1236,7 @@ module.exports.create = function (spec, on_init_ok, on_init_failed) {
     };
 
     that.subscribe = function(participantId, subscriptionId, accessNode, subInfo, subType, on_ok, on_error) {
-        log.debug('subscribe, participantId:', participantId, 'subscriptionId:', subscriptionId, 'accessNode:', accessNode.node, 'subInfo:', JSON.stringify(subInfo));
+        log.debug('subscribe, participantId:', participantId, 'subscriptionId:', subscriptionId, 'accessNode:', accessNode.node, 'subInfo:', JSON.stringify(subInfo), 'subType:', subType);
         if ((!subInfo.audio || (streams[subInfo.audio.from] && streams[subInfo.audio.from].audio) || getViewOfMixStream(subInfo.audio.from))
             && (!subInfo.video || (streams[subInfo.video.from] && streams[subInfo.video.from].video) || getViewOfMixStream(subInfo.video.from))) {
 
@@ -1439,7 +1439,7 @@ module.exports.create = function (spec, on_init_ok, on_init_failed) {
                 }
             };
 
-            var terminal_owner = (subType === 'webrtc' || subType === 'sip') ? participantId : room_id + '-' + randomId();
+            var terminal_owner = (subType === 'webrtc' || subType === 'sip') ? participantId : room_id + '-' + 'common'/*randomId()*/;
             newTerminal(terminal_id, 'participant', terminal_owner, accessNode, function () {
                 doSubscribe();
             }, on_error);
