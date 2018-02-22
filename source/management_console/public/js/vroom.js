148b79252274439e1041ae62c702b64fbfbb4302
Chen, Li1 <li1.chen@intel.com>
2018-02-22 10:18:50 +0800

    Update management console to 4.0
    Change-Id: I9a692d64fcae1094dad8c45cdfe6a6fe1dc400c7
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/194654
    Reviewed-by: webrtctest <webrtctest@intel.com>
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>

---
+++ source/management_console/public/js/vroom.js
@@ -0,0 +1,186 @@
+var audioFormat = {
+  type: 'object',
+  properties: {
+    codec: {
+      type: 'string',
+      enum: [
+        'pcmu', 'pcma', 'opus',
+        'g722', 'isac', 'ilbc',
+        'aac', 'ac3', 'nellymoser'
+      ]
+    },
+    sampleRate: { type: 'number' },
+    channelNum: { type: 'number' }
+  },
+  required: ['codec']
+};
+
+var videoFormat = {
+  type: 'object',
+  properties: {
+    codec: { type: 'string', enum: ['h264', 'h265', 'vp8', 'vp9'] },
+    profile: { type: 'string' }
+  },
+  required: ['codec']
+};
+
+var videoPara = {
+  type: 'object',
+  properties: {
+    resolution: { type: 'array', items: { type: 'string' } },
+    framerate: { type: 'array', items: { type: 'number' } },
+    bitrate: { type: 'array', items: { type: 'string' } },
+    keyFrameInterval: { type: 'array', items: { type: 'number' } }
+  }
+};
+
+var videoParaV = {
+  type: 'object',
+  properties: {
+    resolution: {
+      type: 'object',
+      properties: {
+        width: { type: 'number' },
+        height: { type: 'number' },
+      }
+    },
+    framerate: { type: 'number' },
+    bitrate: { type: 'number' },
+    keyFrameInterval: { type: 'number' }
+  }
+};
+
+var customItem = {
+  type: 'object',
+  properties: {
+    region: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          shape: { type: 'string', enum: ['rectangle'], default: 'rectangle' },
+          area: {
+            type: 'object',
+            properties: {
+              left: { type: 'string' },
+              top: { type: 'string' },
+              width: { type: 'string' },
+              height: { type: 'string' },
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+var viewItem = {
+  type: 'object',
+  properties: {
+    label: { type: 'string', default: 'common' },
+    audio: {
+      type: 'object',
+      properties: {
+        format: audioFormat,
+        vad: { type: 'boolean', default: false }
+      }
+    },
+    video: {
+      type: 'object',
+      properties: {
+        format: videoFormat,
+        parameters: videoParaV,
+        maxInput: { type: 'number' },
+        motionFactor: { type: 'number' },
+        bgColor: {
+          type: 'object',
+          properties: {
+            r: { type: 'number' },
+            g: { type: 'number' },
+            b: { type: 'number' }
+          }
+        },
+        layout: {
+          type: 'object',
+          properties: {
+            fitPolicy: { type: 'string', enum: ['letterbox', 'crop'], default: 'letterbox' },
+            //setRegionEffect: { en}
+            templates: {
+              type: 'object',
+              properties: {
+                base: { type: 'string', enum: ['fluid', 'lecture', 'custom'], default: 'fluid' },
+                custom: {
+                  type: 'array',
+                  items: customItem
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  required: ['label']
+};
+
+var RoomSchema = {
+  type: 'object',
+  properties: {
+    name: { type: 'string' },
+    inputLimit: { type: 'number', minimum: -1 },
+    participantLimit: { type: 'number', minimum: -1 },
+    views: { type: 'array', items: viewItem },
+    mediaIn: {
+      type: 'object',
+      properties: {
+        audio: {
+          type: 'array',
+          items: audioFormat
+        },
+        video: {
+          type: 'array',
+          items: videoFormat
+        }
+      }
+    },
+    mediaOut: {
+      type: 'object',
+      properties: {
+        audio: {
+          type: 'array',
+          items: audioFormat
+        },
+        video: {
+          type: 'object',
+          properties: {
+            format: { type: 'array', items: videoFormat },
+            parameters: videoPara
+          }
+        }
+      }
+    },
+    transcoding: {
+      audio: { type: 'boolean', default: true },
+      video: {
+        format: { type: 'boolean', default: true },
+        parameters: {
+          resolution: { type: 'boolean', default: true },
+          framerate: { type: 'boolean', default: true },
+          bitrate: { type: 'boolean', default: true },
+          keyFrameInterval: { type: 'boolean', default: true }
+        }
+      }
+    },
+    sip: {
+      sipServer: { type: 'string' },
+      username: { type: 'string' },
+      password: { type: 'string' }
+    },
+    notifying: {
+      participantActivities: { type: 'boolean', default: true },
+      streamChange: { type: 'boolean', default: true },
+    }
+  },
+  required: ['name']
+};
