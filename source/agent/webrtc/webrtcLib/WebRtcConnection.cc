e8e0cf3967be4a1f335403b41aa924376454f18c
Chen, Li1 <li1.chen@intel.com>
2018-03-05 13:50:06 +0800

    Enable h265 in webrtc connection settings.
    Change-Id: Icdff6bd0d49957c47f8a81e30a1017793e2c5091
    Reviewed-on: https://git-amr-1.devtools.intel.com/gerrit/195788
    Reviewed-by: Duan, Xiande <xiande.duan@intel.com>
    Tested-by: Duan, Xiande <xiande.duan@intel.com>

--- source/agent/webrtc/webrtcLib/WebRtcConnection.cc
+++ source/agent/webrtc/webrtcLib/WebRtcConnection.cc
@@ -155,6 +155,25 @@ NAN_METHOD(WebRtcConnection::New) {
       rtp_map.feedback_types.push_back("goog-remb");
       rtp_mappings.push_back(rtp_map);
     }
+    // H265
+    {
+      erizo::RtpMap rtp_map;
+      rtp_map.payload_type = 121;
+      rtp_map.encoding_name = "H265";
+      rtp_map.media_type = erizo::VIDEO_TYPE;
+      rtp_map.clock_rate = 90000;
+      rtp_map.channels = 1;
+      rtp_map.format_parameters.insert(rtp_map.format_parameters.begin(),
+              std::pair<std::string, std::string> ("profile-level-id", "42e01f"));
+      rtp_map.format_parameters.insert(rtp_map.format_parameters.begin(),
+              std::pair<std::string, std::string> ("level-asymmetry-allowed", "1"));
+      rtp_map.format_parameters.insert(rtp_map.format_parameters.begin(),
+              std::pair<std::string, std::string> ("packetization-mode", "1"));
+      rtp_map.feedback_types.push_back("ccm fir");
+      rtp_map.feedback_types.push_back("nack");
+      rtp_map.feedback_types.push_back("goog-remb");
+      rtp_mappings.push_back(rtp_map);
+    }
     // RED
     {
       erizo::RtpMap rtp_map;
