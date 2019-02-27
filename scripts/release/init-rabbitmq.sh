84b7b0af3de9652d4bc61cce1a0afb0a8a8332e6
Chen Li1 <li1.chen@intel.com>
2019-02-27 14:23:54 +0800

    Detect "root" user first when apply "sudo" commands

--- scripts/release/init-rabbitmq.sh
+++ scripts/release/init-rabbitmq.sh
@@ -25,6 +25,10 @@ ROOT=`cd "${this}/.."; pwd`
 export WOOGEEN_HOME=${ROOT}
 
 LogDir=${WOOGEEN_HOME}/logs
+SUDO=""
+if [[ $EUID -ne 0 ]]; then
+   SUDO="sudo -E"
+fi
 
 usage() {
   echo
@@ -48,19 +52,19 @@ install_deps() {
     if [[ "$OS" =~ .*6.* ]] # CentOS 6.x
     then
       wget -c http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-      sudo rpm -Uvh epel-release-6*.rpm
+      ${SUDO} rpm -Uvh epel-release-6*.rpm
     elif [[ "$OS" =~ .*7.* ]] # CentOS 7.x
     then
       wget -c http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-      sudo rpm -Uvh epel-release-latest-7*.rpm
+      ${SUDO} rpm -Uvh epel-release-latest-7*.rpm
     fi
-    sudo sed -i 's/https/http/g' /etc/yum.repos.d/epel.repo
-    sudo -E yum install rabbitmq-server -y
+    ${SUDO} sed -i 's/https/http/g' /etc/yum.repos.d/epel.repo
+    ${SUDO} yum install rabbitmq-server -y
   elif [[ "$OS" =~ .*ubuntu.* ]]
   then
     echo -e "\x1b[32mInstalling dependent components and libraries via apt-get...\x1b[0m"
-    sudo apt-get update
-    sudo apt-get install rabbitmq-server
+    ${SUDO} apt-get update
+    ${SUDO} apt-get install rabbitmq-server
   else
     echo -e "\x1b[32mUnsupported platform...\x1b[0m"
   fi
@@ -72,17 +76,17 @@ start_up() {
   # Use default configuration
   if [[ "$OS" =~ .*centos.* ]]
   then
-    if ! sudo systemctl status rabbitmq-server >/dev/null; then
+    if ! ${SUDO} systemctl status rabbitmq-server >/dev/null; then
       echo "Start rabbitmq-server - \"systemctl start rabbitmq-server\""
-      sudo systemctl start rabbitmq-server
+      ${SUDO} systemctl start rabbitmq-server
     else
       echo -e "\x1b[32mRabbitmq-server already running\x1b[0m"
     fi
   elif [[ "$OS" =~ .*ubuntu.* ]]
   then
-    if ! sudo service rabbitmq-server status >/dev/null; then
+    if ! ${SUDO} service rabbitmq-server status >/dev/null; then
       echo "Start rabbitmq-server - \"service rabbitmq-server start\""
-      sudo service rabbitmq-server start
+      ${SUDO} service rabbitmq-server start
     else
       echo -e "\x1b[32mRabbitmq-server already running\x1b[0m"
     fi
