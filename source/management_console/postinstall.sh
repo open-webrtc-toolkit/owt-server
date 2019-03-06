3f13dec31294027b7dba3730521c2b68e3e8cdf5
Chen Li1 <li1.chen@intel.com>
2019-03-06 11:14:45 +0800

    Fix management_console issues, remove third-party js in repo

---
+++ source/management_console/postinstall.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# vendor directory
+mkdir -p public/vendor/css
+mkdir -p public/vendor/js
+mkdir -p public/vendor/fonts
+# jquery
+cp node_modules/jquery/dist/jquery.min.js public/vendor/js/
+# bootstrap
+cp node_modules/bootstrap/dist/css/bootstrap.min.css public/vendor/css/
+cp node_modules/bootstrap/dist/js/bootstrap.min.js public/vendor/js/
+cp -r node_modules/bootstrap/dist/fonts/* public/vendor/fonts
+# lodash
+cp node_modules/lodash/lodash.min.js public/vendor/js/
+# pnotify js & css
+cp node_modules/pnotify/dist/pnotify.css public/vendor/css/
+cp node_modules/pnotify/dist/pnotify.buttons.css public/vendor/css/
+cp node_modules/pnotify/dist/pnotify.js public/vendor/js/
+cp node_modules/pnotify/dist/pnotify.confirm.js public/vendor/js/
+cp node_modules/pnotify/dist/pnotify.buttons.js public/vendor/js/
+# react
+cp node_modules/react/umd/react.production.min.js public/vendor/js/
+cp node_modules/react-dom/umd/react-dom.production.min.js public/vendor/js/
+# react-table
+cp node_modules/react-table/react-table.css public/vendor/css/
+cp node_modules/react-table/react-table.min.js public/vendor/js/
+# crypto-js
+cp node_modules/crypto-js/crypto-js.js public/vendor/js/
