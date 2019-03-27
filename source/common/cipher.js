2264fd83b15d8fa57b0bda7437e5504f6d2162c9
ChenLi <cl.zhejiang.china@gmail.com>
2019-03-28 07:50:04 +0800

    Update to the latest recommanded crypto use (#64)

--- source/common/cipher.js
+++ source/common/cipher.js
@@ -9,16 +9,17 @@ var algorithm = 'aes-256-ctr';
 var stream = require('stream');
 var fs = require('fs');
 var zlib = require('zlib');
+var iv = Buffer.from('3a613563c433a361a62a9efe4dd8462e', 'hex');
 
 function encrypt (password, text){
-  var cipher = crypto.createCipher(algorithm, password);
+  var cipher = crypto.createCipheriv(algorithm, password, iv);
   var enc = cipher.update(text, 'utf8', 'hex');
   enc += cipher.final('hex');
   return enc;
 }
  
 function decrypt (password, text){
-  var decipher = crypto.createDecipher(algorithm, password);
+  var decipher = crypto.createDecipheriv(algorithm, password, iv);
   var dec = decipher.update(text, 'hex', 'utf8');
   dec += decipher.final('utf8');
   return dec;
@@ -36,7 +37,7 @@ function lock (password, object, filename, cb) {
   out.on('finish', function () {
     cb(null);
   });
-  s.pipe(zlib.createGzip()).pipe(crypto.createCipher(algorithm, password)).pipe(out);
+  s.pipe(zlib.createGzip()).pipe(crypto.createCipheriv(algorithm, password, iv)).pipe(out);
 }
 
 function unlock (password, filename, cb) {
@@ -55,14 +56,14 @@ function unlock (password, filename, cb) {
   unzip.on('error', function (e) {
     cb(e);
   });
-  s.pipe(crypto.createDecipher(algorithm, password)).pipe(unzip);
+  s.pipe(crypto.createDecipheriv(algorithm, password, iv)).pipe(unzip);
 }
 
 module.exports = {
   encrypt: encrypt,
   decrypt: decrypt,
   // Replace k with your key generator
-  k: crypto.pbkdf2Sync('woogeen', 'mcu', 4000, 128, 'sha1'),
+  k: Buffer.from('3d84a1efc77268c98bc6ca2921eb35a6d82a40a38696d25fbb64ff1733cd2523', 'hex'),
   astore: '.owt.authstore',
   kstore: '.owt.keystore',
   lock: lock,
