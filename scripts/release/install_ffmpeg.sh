1f4e145db925ab7c757ac4a3fdadc3fe2066a6f6
Chen Li1 <li1.chen@intel.com>
2019-05-21 17:04:57 +0800

    Add install_ffmpeg.sh

---
+++ scripts/release/install_ffmpeg.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+# Copyright (C) <2019> Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+this=`dirname "$0"`
+this=`cd "$this"; pwd`
+SUDO=""
+if [[ $EUID -ne 0 ]]; then
+   SUDO="sudo -E"
+fi
+
+detect_OS() {
+  lsb_release >/dev/null 2>/dev/null
+  if [ $? = 0 ]
+  then
+    lsb_release -ds | sed 's/^\"//g;s/\"$//g'
+  # a bunch of fallbacks if no lsb_release is available
+  # first trying /etc/os-release which is provided by systemd
+  elif [ -f /etc/os-release ]
+  then
+    source /etc/os-release
+    if [ -n "${PRETTY_NAME}" ]
+    then
+      printf "${PRETTY_NAME}\n"
+    else
+      printf "${NAME}"
+      [[ -n "${VERSION}" ]] && printf " ${VERSION}"
+      printf "\n"
+    fi
+  # now looking at distro-specific files
+  elif [ -f /etc/arch-release ]
+  then
+    printf "Arch Linux\n"
+  elif [ -f /etc/gentoo-release ]
+  then
+    cat /etc/gentoo-release
+  elif [ -f /etc/fedora-release ]
+  then
+    cat /etc/fedora-release
+  elif [ -f /etc/redhat-release ]
+  then
+    cat /etc/redhat-release
+  elif [ -f /etc/debian_version ]
+  then
+    printf "Debian GNU/Linux " ; cat /etc/debian_version
+  else
+    printf "Unknown\n"
+  fi
+}
+
+install_build_deps() {
+  local OS=$(detect_OS | awk '{print tolower($0)}')
+  echo $OS
+
+  if [[ "$OS" =~ .*centos.* ]]
+  then
+    echo -e "\x1b[32mInstalling dependent components and libraries via yum...\x1b[0m"
+    ${SUDO} yum install gcc gcc-c++ nasm yasm freetype-devel -y
+  elif [[ "$OS" =~ .*ubuntu.* ]]
+  then
+    echo -e "\x1b[32mInstalling dependent components and libraries via apt-get...\x1b[0m"
+    ${SUDO} apt-get update
+    ${SUDO} apt-get install make gcc g++ nasm yasm libfreetype6-dev
+  else
+    echo -e "\x1b[32mUnsupported platform...\x1b[0m"
+  fi
+}
+
+install_ffmpeg(){
+  local VERSION="4.1.3"
+  local DIR="ffmpeg-${VERSION}"
+  local SRC="${DIR}.tar.bz2"
+  local SRC_URL="http://ffmpeg.org/releases/${SRC}"
+  local SRC_MD5SUM="9985185a8de3678e5b55b1c63276f8b5"
+  local PREFIX_DIR="${this}/ffmpeg-install"
+
+  local LIST_LIBS=`ls ${this}/lib/libav* 2>/dev/null`
+  pushd ${this} >/dev/null
+  [[ ! -z $LIST_LIBS ]] && echo "ffmpeg already installed." && return 0
+
+  [[ ! -s ${SRC} ]] && wget -c ${SRC_URL}
+  if ! (echo "${SRC_MD5SUM} ${SRC}" | md5sum --check) ; then
+      echo "Downloaded file ${SRC} is corrupted."
+      rm -v ${SRC}
+      return 1
+  fi
+  rm -fr ${DIR}
+  tar xf ${SRC}
+  pushd ${DIR} >/dev/null
+  CFLAGS=-fPIC ./configure --prefix=${PREFIX_DIR} --enable-shared \
+    --disable-static --disable-libvpx --disable-vaapi --enable-libfreetype
+  make -j4 -s V=0 && make install
+  popd
+  popd
+
+  cp -P ${PREFIX_DIR}/lib/*.so.* ${this}/lib/
+  cp -P ${PREFIX_DIR}/lib/*.so ${this}/lib/
+}
+
+echo "Install building dependencies..."
+install_build_deps
+
+echo "Install ffmpeg..."
+install_ffmpeg
