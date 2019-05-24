ce9eec3e853a4009544d18a5ccb42e8626314531
Xiande <xiande.duan@intel.com>
2019-05-24 10:50:40 +0800

    Bugfix: internal.ip_address does not take effect in xxx_agent/agent.toml

--- source/agent/audio/index.js
+++ source/agent/audio/index.js
@@ -167,7 +167,7 @@ module.exports = function (rpcClient, selfRpcId, parentRpcId, clusterWorkerIP) {
         internalOpt.minport = global.config.internal.minport;
         internalOpt.maxport = global.config.internal.maxport;
         var portInfo = internalConnFactory.create(connectionId, direction, internalOpt);
-        callback('callback', {ip: that.clusterIP, port: portInfo});
+        callback('callback', {ip: global.config.internal.ip_address, port: portInfo});
     };
 
     that.destroyInternalConnection = function (connectionId, direction, callback) {
