d9731c9b8c8004633cceb56a18e2420bbdc28a51
Xiaofeng Wang <wasphin@gmail.com>
2019-07-15 09:42:39 +0800

    install libs one by one to build docker easier (#180)

---
+++ scripts/installDepsEnv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT=`readlink -e ${BASH_SOURCE[0]}`
+FILENAME=`basename $SCRIPT`
+PATHNAME=`dirname $SCRIPT`
+ROOT=$PATHNAME/..
+BUILD_DIR=$ROOT/build
+CURRENT_DIR=`pwd`
+
+LIB_DIR=$BUILD_DIR/libdeps
+PREFIX_DIR=$LIB_DIR/build/
+DISABLE_NONFREE=true
+CLEANUP=false
+NIGHTLY=false
+NO_INTERNAL=false
+INCR_INSTALL=false
+
+. ${PATHNAME}/installCommonDeps.sh
