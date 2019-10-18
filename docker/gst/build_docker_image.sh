839591d17318cb76b308ccb4758397f6a5bfa146
Qiujiao <qiujiao.wu@intel.com>
2019-10-18 15:57:10 +0800

    Remove debug code (#322)
    * Add Dockerfile for gst analytics

---
+++ docker/gst/build_docker_image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ==============================================================================
+# Copyright (C) 2018-2019 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+# ==============================================================================
+
+build_type=${1:-opensource}
+tag=${2:-latest}
+
+
+BASEDIR=$(dirname "$0")
+
+docker build --target owt-build -t gst-owt:build \
+    --build-arg http_proxy=${HTTP_PROXY} \
+    --build-arg https_proxy=${HTTPS_PROXY} \
+    .
+
+docker build --target owt-run -t gst-owt:run \
+    --build-arg http_proxy=${HTTP_PROXY} \
+    --build-arg https_proxy=${HTTPS_PROXY} \
+    .
+
+docker build --target analytics-run -t gst-analytics:run \
+    --build-arg http_proxy=${HTTP_PROXY} \
+    --build-arg https_proxy=${HTTPS_PROXY} \
+    .
--- source/agent/addons/resourceUtil/ResourceUtil.cc
+++ source/agent/addons/resourceUtil/ResourceUtil.cc
@@ -14,45 +14,19 @@ namespace mcu {
 DEFINE_LOGGER(ResourceUtil, "mcu.media.ResourceUtil");
 
 ResourceUtil::ResourceUtil() {
-    //m_hddlClient = new hddl::HddlClient("HDDLMetric");
 }
 
 ResourceUtil::~ResourceUtil() {
-    //delete m_hddlClient;
 }
 
 float ResourceUtil::getVPUUtil() { 
 
-    #if 0
-    if (m_hddlClient->checkInitStatus() != hddl::HDDL_OK) {
-        ELOG_ERROR("Error: checkInitStatus failed.\n");
-        return -1;
-    }
-
-    hddl::HddlQuery query;
-    hddl::QueryType type = hddl::QUERY_TYPE_ALL;
-    hddl::HddlStatusCode hddlStatus = m_hddlClient->query(type, &query);
-    if (hddlStatus < 0) {
-        ELOG_ERROR("Error: Query device info error, retCode= %d \n", hddlStatus);
-        return -1;
-    }
-    //query.dump();
-
-    std::vector<float> v = query.getDeviceUtilization();
-    float sum=0, avg;
-    for(float n : v) {
-            sum +=n;
-    }
-    avg = sum/v.size();
-    #else
-    //Core ie;
     std::vector<float> v = ie.GetMetric("HDDL", VPU_HDDL_METRIC(DEVICE_UTILIZATION));
     float sum=0, avg=0;
     for(float n : v) {
             sum +=n;
     }
     avg = sum/v.size();
-    #endif
 
     return avg;
 }
