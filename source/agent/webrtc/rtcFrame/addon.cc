b24938188f1cb998f7350dbd44e535d18b6b0bc5
Jianjun Zhu <jianjun.zhu@intel.com>
2020-06-02 14:13:09 +0800

    Fix build issues after rebasing.

--- source/agent/webrtc/rtcFrame/addon.cc
+++ source/agent/webrtc/rtcFrame/addon.cc
@@ -6,20 +6,12 @@
 #include "AudioFramePacketizerWrapper.h"
 #include "VideoFrameConstructorWrapper.h"
 #include "VideoFramePacketizerWrapper.h"
-#include "WebRtcConnection.h"
-#include "ThreadPool.h"
-#include "IOThreadPool.h"
-#include "MediaStream.h"
 
 #include <node.h>
 
 using namespace v8;
 
 void InitAll(Handle<Object> exports) {
-  WebRtcConnection::Init(exports);
-  MediaStream::Init(exports);
-  ThreadPool::Init(exports);
-  IOThreadPool::Init(exports);
   AudioFrameConstructor::Init(exports);
   AudioFramePacketizer::Init(exports);
   VideoFrameConstructor::Init(exports);
