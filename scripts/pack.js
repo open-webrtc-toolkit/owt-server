b2198c69bca6f77ffd8c6d225b37235a6bb5a1d2
Jianjun Zhu <jianjun.zhu@intel.com>
2020-06-03 10:44:35 +0800

    Don't start quic-agent by default.

--- scripts/pack.js
+++ scripts/pack.js
@@ -40,6 +40,8 @@ const originCwd = cwd();
 const osScript = path.join(rootDir, 'scripts/detectOS.sh');
 const osType = execSync(`bash ${osScript}`).toString().toLowerCase();
 
+const experimentalTargets = ['quic-agent'];
+
 var allTargets = [];
 
 if (options.full) {
@@ -132,7 +134,7 @@ function getPackList(targets) {
 
   var packList = targets.filter((element) => {
     // Don't include QUIC agent by default until CI is added for QUIC SDK.
-    if (options.target.includes('all') && element.rules.name !== 'quic-agent') return true;
+    if (options.target.includes('all') && !experimentalTargets.includes(element.rules.name)) return true;
     return options.target.includes(element.rules.name);
   });
   if (packList.length === 0) {
@@ -599,6 +601,9 @@ function packScripts() {
   }
   scriptItems.push('app');
   scriptItems.forEach((m) => {
+    if (experimentalTargets.includes(m)) {
+      return;
+    }
     startCommands += '${bin}/daemon.sh start ' + m + ' $1\n';
     stopCommands += '${bin}/daemon.sh stop ' + m + '\n';
   });
