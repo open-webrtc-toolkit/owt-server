58a69a447cdb0a9a85fac59d64322f42b1b85a66
Chen Li1 <li1.chen@intel.com>
2020-06-08 16:01:34 +0800

    Update license and date

--- source/core/rtc_adapter/VieReceiver.h
+++ source/core/rtc_adapter/VieReceiver.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, The WebRTC project authors. All rights reserved.
+ * Copyright (c) 2013, The WebRTC project authors. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -29,6 +29,10 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+// Copyright (C) <2017> Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
 
 #ifndef WEBRTC_VIDEO_ENGINE_VIE_RECEIVER_H_
 #define WEBRTC_VIDEO_ENGINE_VIE_RECEIVER_H_
