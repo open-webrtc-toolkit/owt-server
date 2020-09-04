8f46b1bed44b618ff23292d554aaadba8056a184
qwu16 <qiujiao.wu@intel.com>
2020-09-04 14:13:44 +0800

    Refine format

--- source/cluster_manager/matcher.js
+++ source/cluster_manager/matcher.js
@@ -9,7 +9,6 @@ var logger = require('./logger').logger;
 var log = logger.getLogger('Matcher');
 
 var is_isp_applicable = function (supported_isps, preferred_isp) {
-    log.info("supported isps are:", supported_isps);
     return supported_isps.indexOf(preferred_isp) !== -1 || supported_isps.length === 0;
 };
 
@@ -81,7 +80,6 @@ var videoMatcher = function () {
         for (var i in availableCandidates) {
             var id = availableCandidates[i];
             var capacity = workers[id].info.capacity;
-	    console.log("Get video preference is:", preference.origin);
             if (is_isp_applicable(capacity.isps, preference.origin.isp)) {
                 if (is_region_suited(capacity.regions, preference.origin.region)) {
                     if (!found_sweet) {
@@ -124,7 +122,32 @@ var generalMatcher = function () {
     };
 };
 
-var audioMatcher = webrtcMatcher;
+var audioMatcher = function () {
+    this.match = function (preference, workers, candidates) {
+        var result = [],
+            found_sweet = false;
+        for (var i in candidates) {
+            var id = candidates[i];
+            var capacity = workers[id].info.capacity;
+            if (is_isp_applicable(capacity.isps, preference.origin.isp)) {
+                if (is_region_suited(capacity.regions, preference.origin.region)) {
+                    if (!found_sweet) {
+                        found_sweet = true;
+                        result = [id];
+                    } else {
+                        result.push(id);
+                    }
+                } else {
+                    if (!found_sweet) {
+                        result.push(id);
+                    }
+                }
+            }
+        }
+        return result;
+    };
+};
+
 var streamingMatcher = webrtcMatcher;
 var portalMatcher = webrtcMatcher;
 
