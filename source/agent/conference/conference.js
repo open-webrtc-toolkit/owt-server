43ff41fc0bcdd6cbc6d1a6a68062ad70ec4d8ca3
qwu16 <qiujiao.wu@intel.com>
2020-09-04 16:18:24 +0800

    Fix CI failed issue

--- source/agent/conference/conference.js
+++ source/agent/conference/conference.js
@@ -2191,7 +2191,8 @@ var Conference = function (rpcClient, selfRpcId) {
         }
 
         // Schedule preference for worker node
-        var accessPreference = Object.assign({}, streams[subDesc.media.video.from].info.origin);
+        var streamFrom = subDesc.media.video.from === undefined?subDesc.media.audio.from:subDesc.media.video.from;
+        var accessPreference = Object.assign({}, streams[streamFrom].info.origin);
         if (subDesc.type === 'analytics') {
           // Schedule analytics agent according to the algorithm
           accessPreference.algorithm = subDesc.connection.algorithm;
