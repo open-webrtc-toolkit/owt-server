3fb208bf74eea0bfa621c23cec6a287fc6f14629
Your Name <you@example.com>
2020-09-14 09:43:47 +0800

    Use GStreamer and openvino for video analytics

---
+++ source/agent/analytics/videoGstPipeline/InternalIn.h
@@ -0,0 +1,120 @@
+// Copyright (C) <2019> Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef InternalIn_h
+#define InternalIn_h
+
+#include <gst/gst.h>
+#include <gst/app/gstappsrc.h>
+#include <logger.h>
+
+#include "RawTransport.h"
+
+
+enum FrameFormat {
+    FRAME_FORMAT_UNKNOWN    = 0,
+
+    FRAME_FORMAT_I420       = 100,
+
+    FRAME_FORMAT_VP8        = 200,
+    FRAME_FORMAT_VP9,
+    FRAME_FORMAT_H264,
+    FRAME_FORMAT_H265,
+
+    FRAME_FORMAT_MSDK       = 300,
+
+    FRAME_FORMAT_PCM_48000_2    = 800,
+
+    FRAME_FORMAT_PCMU       = 900,
+    FRAME_FORMAT_PCMA,
+    FRAME_FORMAT_OPUS,
+    FRAME_FORMAT_ISAC16,
+    FRAME_FORMAT_ISAC32,
+    FRAME_FORMAT_ILBC,
+    FRAME_FORMAT_G722_16000_1,
+    FRAME_FORMAT_G722_16000_2,
+
+    FRAME_FORMAT_AAC,           // ignore sample rate and channels for decoder, default is 48000_2
+    FRAME_FORMAT_AAC_48000_2,   // specify sample rate and channels for encoder
+
+    FRAME_FORMAT_AC3,
+    FRAME_FORMAT_NELLYMOSER,
+};
+
+struct VideoFrameSpecificInfo {
+    uint16_t width;
+    uint16_t height;
+    bool isKeyFrame;
+};
+
+struct AudioFrameSpecificInfo {
+    /*AudioFrameSpecificInfo() : isRtpPacket(false) {}*/
+    uint8_t isRtpPacket; // FIXME: Temporarily use Frame to carry rtp-packets due to the premature AudioFrameConstructor implementation.
+    uint32_t nbSamples;
+    uint32_t sampleRate;
+    uint8_t channels;
+};
+
+typedef union MediaSpecInfo {
+    VideoFrameSpecificInfo video;
+    AudioFrameSpecificInfo audio;
+} MediaSpecInfo;
+
+struct Frame {
+    FrameFormat     format;
+    uint8_t*        payload;
+    uint32_t        length;
+    uint32_t        timeStamp;
+    MediaSpecInfo   additionalInfo;
+};
+
+enum FeedbackType {
+    VIDEO_FEEDBACK,
+    AUDIO_FEEDBACK
+};
+
+enum FeedbackCmd {
+    REQUEST_KEY_FRAME,
+    SET_BITRATE,
+    RTCP_PACKET  // FIXME: Temporarily use FeedbackMsg to carry audio rtcp-packets due to the premature AudioFrameConstructor implementation.
+};
+
+struct FeedbackMsg {
+    FeedbackType type;
+    FeedbackCmd  cmd;
+    union {
+        unsigned short kbps;
+        struct RtcpPacket{// FIXME: Temporarily use FeedbackMsg to carry audio rtcp-packets due to the premature AudioFrameConstructor implementation.
+            uint32_t len;
+            char     buf[128];
+        } rtcp;
+    } data;
+    FeedbackMsg(FeedbackType t, FeedbackCmd c) : type{t}, cmd{c} {}
+};
+
+class InternalIn : public owt_base::RawTransportListener{
+    DECLARE_LOGGER();
+public:
+    InternalIn(GstAppSrc *data, unsigned int minPort = 0, unsigned int maxPort = 0);
+    virtual ~InternalIn();
+
+    unsigned int getListeningPort();
+
+    // Implements FrameSource
+    void onFeedback(const FeedbackMsg&);
+
+    // Implements RawTransportListener.
+    void onTransportData(char* buf, int len);
+    void onTransportError() { }
+    void onTransportConnected() { }
+    void setPushData(bool status);
+
+private:
+    bool m_start;
+    bool m_needKeyFrame;
+    GstAppSrc *appsrc;
+    boost::shared_ptr<owt_base::RawTransportInterface> m_transport;
+};
+
+#endif /* InternalIn_h */
---
+++ source/agent/analytics/videoGstPipeline/VideoGstAnalyzer.h
@@ -0,0 +1,92 @@
+// Copyright (C) <2019> Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef VideoGstAnalyzer_H
+#define VideoGstAnalyzer_H
+
+#include <gst/gst.h>
+#include <glib-object.h>
+#include <gst/pbutils/encoding-profile.h>
+#include <gst/app/gstappsink.h>
+#include <string>
+#include <boost/thread.hpp>
+#include <logger.h>
+#include "InternalIn.h"
+#include <InternalOut.h>
+#include "AnalyticsPipeline.h"
+#include <stdio.h>
+
+namespace mcu {
+
+class VideoGstAnalyzer{
+    DECLARE_LOGGER();
+public:
+    VideoGstAnalyzer();
+    virtual ~VideoGstAnalyzer();
+    int createPipeline();
+    void clearPipeline();
+    int getListeningPort();
+    void emitListenTo(int minPort,int maxPort);
+    int setPlaying();
+
+    void emitConnectTo(int connectionID, char* ip, int remotePort);
+
+    int addElementMany();
+
+    void setOutputParam(std::string codec, int width, int height, 
+    int framerate, int bitrate, int kfi, std::string algo, std::string pluginName);
+
+    void stopLoop();
+
+    void disconnect(owt_base::InternalOut* out);
+
+    void addOutput(int connectionID, owt_base::InternalOut* out);
+
+    static void pad_added_handler(GstElement *src, GstPad *new_pad, GstElement *data);
+    static void on_pad_added (GstElement *element, GstPad *pad, gpointer data);
+
+    static void start_feed (GstElement * source, guint size, gpointer data);
+    static gboolean push_data (gpointer data);
+    static void stop_feed (GstElement * source, gpointer data);
+    static void new_sample_from_sink (GstElement * source, gpointer data);
+
+protected:
+    static void main_loop_thread(gpointer);
+    static GMainLoop *loop;
+    static gboolean StreamEventCallBack(GstBus *bus, GstMessage *message, gpointer data);
+    void setState();
+    boost::scoped_ptr<InternalIn> m_internalin;
+    std::list<owt_base::InternalOut*> m_internalout;
+    guint sourceid;
+
+private:
+    GstElement *pipeline, *source, *sink;
+    GstPad *encoder_pad;
+    void* pipelineHandle;
+    rvaPipeline* pipeline_;
+    rva_create_t* createPlugin;
+    rva_destroy_t* destroyPlugin;
+    
+    GstStateChangeReturn ret;
+
+    GThread *m_thread;
+    guint m_bus_watch_id;
+    GstBus *m_bus;
+    boost::thread m_playthread;
+
+    int connectPort;
+
+    //param
+    std::string codec;
+    std::string algo,libraryName;
+    std::string resolution;
+    int width,height;
+    int framerate,bitrate;
+    int kfi; //keyFrameInterval
+    FILE *fp;
+};
+
+}
+
+#endif //VideoGstAnalyzer_H
