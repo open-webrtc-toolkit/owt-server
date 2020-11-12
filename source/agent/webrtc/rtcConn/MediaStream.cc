5fc8bf2bf7c758f0ef6cc800a261c9bb8357804b
ChenLi <li1.chen@intel.com>
2020-11-12 15:48:12 +0800

    Fix random crash due to threading (#691)

--- source/agent/webrtc/rtcConn/MediaStream.cc
+++ source/agent/webrtc/rtcConn/MediaStream.cc
@@ -177,10 +177,10 @@ NAN_METHOD(MediaStream::New) {
 
     bool is_publisher = info[5]->BooleanValue();
 
-    std::shared_ptr<erizo::Worker> worker = thread_pool->me->getLessUsedWorker();
 
     MediaStream* obj = new MediaStream();
-    obj->me = std::make_shared<erizo::MediaStream>(worker, wrtc, wrtc_id, stream_label, is_publisher);
+    // Share same worker with connection
+    obj->me = std::make_shared<erizo::MediaStream>(wrtc->getWorker(), wrtc, wrtc_id, stream_label, is_publisher);
     obj->msink = obj->me.get();
     obj->msource = obj->me.get();
     obj->id_ = wrtc_id;
