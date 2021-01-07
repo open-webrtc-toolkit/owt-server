12d3099e03fc42d129cc9cb1df2094ccab37d71e
Qiujiao <qiujiao.wu@intel.com>
2021-01-07 13:34:46 +0800

    Fix socket.io 2.4.0 cors issue (#852)
    * Fix socketio 2.4.0 cors connection issue 847

--- source/portal/socketIOServer.js
+++ source/portal/socketIOServer.js
@@ -352,14 +352,21 @@ var SocketIOServer = function(spec, portal, observer) {
     sioOptions.pingTimeout = spec.pingTimeout * 1000;
   }
 
-  var startInsecure = function(port) {
+  var startInsecure = function(port, cors) {
     var server = require('http').createServer().listen(port);
     io = require('socket.io').listen(server, sioOptions);
+    io.origins((origin, callback) => {
+      if (cors.indexOf(origin) < 0 && cors.indexOf('*') < 0) {
+        return callback('origin not allowed', false);
+      }
+
+      callback(null, true);
+    });
     run();
     return Promise.resolve('ok');
   };
 
-  var startSecured = function(port, keystorePath, forceTlsv12) {
+  var startSecured = function(port, cors, keystorePath, forceTlsv12) {
     return new Promise(function(resolve, reject) {
       var cipher = require('./cipher');
       var keystore = path.resolve(path.dirname(keystorePath), cipher.kstore);
@@ -372,6 +379,12 @@ var SocketIOServer = function(spec, portal, observer) {
           }
           var server = require('https').createServer(option).listen(port);
           io = require('socket.io').listen(server, sioOptions);
+          io.origins((origin, callback) => {
+            if (cors.indexOf(origin) < 0 && cors.indexOf('*') < 0) {
+              return callback('origin not allowed', false);
+            }
+            callback(null, true);
+          });
           run();
           resolve('ok');
         } else {
@@ -417,9 +430,9 @@ var SocketIOServer = function(spec, portal, observer) {
 
   that.start = function() {
     if (!spec.ssl) {
-      return startInsecure(spec.port);
+      return startInsecure(spec.port, spec.cors);
     } else {
-      return startSecured(spec.port, spec.keystorePath, spec.forceTlsv12);
+      return startSecured(spec.port, spec.cors, spec.keystorePath, spec.forceTlsv12);
     }
   };
 
