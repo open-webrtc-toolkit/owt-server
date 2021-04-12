7ce60fe5aa2fd534619c13038368ce38d487e9e2
DWJ01 <1521974869@qq.com>
2021-04-13 07:54:07 +0800

    Fix dependency installation

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -2,7 +2,10 @@
 
 install_apt_deps(){
   ${SUDO} apt-get update -y
-  ${SUDO} apt-get install git make gcc g++ libglib2.0-dev pkg-config libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx-dev rabbitmq-server mongodb openjdk-8-jre curl libboost-test-dev nasm yasm gyp libx11-dev libkrb5-dev intel-gpu-tools m4 autoconf libtool automake cmake libfreetype6-dev libgstreamer-plugins-base1.0-dev -y
+  ${SUDO} apt-get install git make gcc g++ libglib2.0-dev docbook2x pkg-config -y
+  ${SUDO} apt-get install libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx-dev rabbitmq-server -y
+  ${SUDO} apt-get install mongodb openjdk-8-jre curl libboost-test-dev nasm yasm gyp libx11-dev libkrb5-dev intel-gpu-tools -y
+  ${SUDO} apt-get install m4 autoconf libtool automake cmake libfreetype6-dev libgstreamer-plugins-base1.0-dev -y
 }
 
 install_mediadeps_nonfree(){
