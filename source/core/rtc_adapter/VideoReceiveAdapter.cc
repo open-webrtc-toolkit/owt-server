fd827633cde3beb40bf76bc38dfc114355be2229
Chen Li1 <li1.chen@intel.com>
2021-09-17 14:53:58 +0800

    Update layer selecting logic for VP9 SVC stream

--- source/core/rtc_adapter/VideoReceiveAdapter.cc
+++ source/core/rtc_adapter/VideoReceiveAdapter.cc
@@ -5,6 +5,7 @@
 #include "VideoReceiveAdapter.h"
 
 #include <future>
+#include <modules/rtp_rtcp/source/video_rtp_depacketizer_vp9.h>
 #include <modules/video_coding/include/video_error_codes.h>
 #include <modules/video_coding/timing.h>
 #include <rtc_base/time_utils.h>
@@ -47,18 +48,6 @@ int32_t VideoReceiveAdapterImpl::AdapterDecoder::InitDecode(const webrtc::VideoC
     }
     return 0;
 }
-
-struct DataPacket {
-    DataPacket() = default;
-    DataPacket(const char* data_, int length_)
-        : length{ length_ }
-    {
-        memcpy(data, data_, length_);
-    }
-
-    char data[1500];
-    int length;
-};
 
 int32_t VideoReceiveAdapterImpl::AdapterDecoder::Decode(const webrtc::EncodedImage& encodedImage,
     bool missing_frames,
@@ -110,26 +99,6 @@ int32_t VideoReceiveAdapterImpl::AdapterDecoder::Decode(const webrtc::EncodedIma
     frame.additionalInfo.video.height = m_height;
     frame.additionalInfo.video.isKeyFrame =
         (encodedImage._frameType == webrtc::VideoFrameType::kVideoFrameKey);
-
-    int spatialId = m_parent ? m_parent->m_preferredSpatialId : -1;
-    if (format == FRAME_FORMAT_VP9 && spatialId >= 0 && encodedImage.SpatialIndex()) {
-        // Drop unwanted layer frame
-        size_t preferredSpatial = spatialId;
-        size_t spatialIndex = encodedImage.SpatialIndex().value_or(0);
-        size_t maxSpatialIndex = std::min(preferredSpatial, spatialIndex);
-        size_t choosenSize = 0;
-        for (size_t i = 0; i <= maxSpatialIndex; i++) {
-            auto frameSize = encodedImage.SpatialLayerFrameSize(i);
-            if (frameSize) {
-                choosenSize += (*frameSize);
-            }
-        }
-        if (choosenSize > 0) {
-            RTC_DLOG(LS_VERBOSE) << "Drop higher layers, before:" << frame.length
-                                 << " after:" << choosenSize;
-            frame.length = choosenSize;
-        }
-    }
 
     if (m_parent) {
         if (m_parent->m_frameListener) {
@@ -320,11 +289,43 @@ std::unique_ptr<webrtc::VideoDecoder> VideoReceiveAdapterImpl::CreateVideoDecode
 
 int VideoReceiveAdapterImpl::onRtpData(char* data, int len)
 {
-    std::shared_ptr<DataPacket> wp = std::make_shared<DataPacket>(data, len);
-    taskQueue()->PostTask([this, wp]() {
+    rtc::CopyOnWriteBuffer buffer(data, len);
+    if (m_format == FRAME_FORMAT_VP9 &&
+        (m_preferredTemporalId >= 0 || m_preferredSpatialId >= 0)) {
+        webrtc::RtpPacket rtpPacket;
+        webrtc::RTPVideoHeader video_header;
+        if (rtpPacket.Parse((const uint8_t*) data, len)) {
+            if (webrtc::VideoRtpDepacketizerVp9::ParseRtpPayload(
+                rtpPacket.PayloadBuffer(), &video_header) > 0) {
+                webrtc::RTPVideoHeaderVP9* vp9_header =
+                    absl::get_if<webrtc::RTPVideoHeaderVP9>(
+                        &(video_header.video_type_header));
+                if (vp9_header) {
+                    if ((m_preferredSpatialId >= 0 &&
+                        vp9_header->spatial_idx <= kMaxSpatialLayers &&
+                        vp9_header->spatial_idx > m_preferredSpatialId) ||
+                        (m_preferredTemporalId >= 0 &&
+                        vp9_header->temporal_idx <= kMaxTemporalLayers &&
+                        vp9_header->temporal_idx > m_preferredTemporalId)) {
+                        return len;
+                    }
+                    if (vp9_header->spatial_idx == m_preferredSpatialId &&
+                        vp9_header->end_of_frame && !rtpPacket.Marker()) {
+                        // Set Marker
+                        rtpPacket.SetMarker(true);
+                        buffer = rtpPacket.Buffer();
+                    }
+                }
+            } else {
+                // Empty payload, vp9 parse fail
+            }
+        }
+    }
+
+    taskQueue()->PostTask([this, buffer]() {
         call()->Receiver()->DeliverPacket(
             webrtc::MediaType::VIDEO,
-            rtc::CopyOnWriteBuffer(wp->data, wp->length),
+            buffer,//rtc::CopyOnWriteBuffer(wp->data, wp->length),
             rtc::TimeUTCMicros());
     });
     return len;
