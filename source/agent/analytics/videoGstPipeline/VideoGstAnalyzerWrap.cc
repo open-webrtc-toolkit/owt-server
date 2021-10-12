db1867a2a15f3e727447ce1fa98576e2c5c2bd01
Chen Li1 <li1.chen@intel.com>
2021-10-12 10:58:17 +0800

    Make analytic addons compatible for higher version node

--- source/agent/analytics/videoGstPipeline/VideoGstAnalyzerWrap.cc
+++ source/agent/analytics/videoGstPipeline/VideoGstAnalyzerWrap.cc
@@ -16,11 +16,11 @@ Persistent<Function> VideoGstAnalyzerWrap::constructor;
 VideoGstAnalyzerWrap::VideoGstAnalyzerWrap() {};
 VideoGstAnalyzerWrap::~VideoGstAnalyzerWrap() {};
 
-void VideoGstAnalyzerWrap::Init(Handle<Object> exports, Handle<Object> module) {
+void VideoGstAnalyzerWrap::Init(Local<Object> exports, Local<Object> module) {
   Isolate* isolate = exports->GetIsolate();
   // Prepare constructor template
   Local<FunctionTemplate> tpl = FunctionTemplate::New(isolate, New);
-  tpl->SetClassName(String::NewFromUtf8(isolate, "VideoGstAnalyzer"));
+  tpl->SetClassName(Nan::New("VideoGstAnalyzer").ToLocalChecked());
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
   // Prototype
   NODE_SET_PROTOTYPE_METHOD(tpl, "close", close);
@@ -31,8 +31,8 @@ void VideoGstAnalyzerWrap::Init(Handle<Object> exports, Handle<Object> module) {
   NODE_SET_PROTOTYPE_METHOD(tpl, "linkInput", linkInput);
   NODE_SET_PROTOTYPE_METHOD(tpl, "addEventListener", addEventListener);
 
-  constructor.Reset(isolate, tpl->GetFunction());
-  module->Set(String::NewFromUtf8(isolate, "exports"), tpl->GetFunction());
+  constructor.Reset(isolate, Nan::GetFunction(tpl).ToLocalChecked());
+  Nan::Set(module, Nan::New("exports").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
 }
 
 void VideoGstAnalyzerWrap::New(const FunctionCallbackInfo<Value>& args) {
@@ -60,24 +60,24 @@ void VideoGstAnalyzerWrap::createPipeline(const FunctionCallbackInfo<Value>& arg
   VideoGstAnalyzerWrap* obj = ObjectWrap::Unwrap<VideoGstAnalyzerWrap>(args.Holder());
   mcu::VideoGstAnalyzer* me = obj->me;
 
-  String::Utf8Value param0(args[0]->ToString());
+  Nan::Utf8String param0(Nan::To<v8::String>(args[0]).ToLocalChecked());
   std::string codec = std::string(*param0);
 
-  String::Utf8Value param1(args[1]->ToString());
+  Nan::Utf8String param1(Nan::To<v8::String>(args[1]).ToLocalChecked());
   std::string resolution = std::string(*param1);
   owt_base::VideoSize vSize{0, 0};
   owt_base::VideoResolutionHelper::getVideoSize(resolution, vSize);
   unsigned int width = vSize.width;
   unsigned int height = vSize.height;
 
-  unsigned int framerateFPS = args[2]->Uint32Value();
-  unsigned int bitrateKbps = args[3]->Uint32Value();
-  unsigned int keyFrameIntervalSeconds = args[4]->Uint32Value();
+  unsigned int framerateFPS = Nan::To<uint32_t>(args[2]).FromJust();
+  unsigned int bitrateKbps = Nan::To<uint32_t>(args[3]).FromJust();
+  unsigned int keyFrameIntervalSeconds = Nan::To<uint32_t>(args[4]).FromJust();
 
-  String::Utf8Value param6(args[5]->ToString());
+  Nan::Utf8String param6(Nan::To<v8::String>(args[5]).ToLocalChecked());
   std::string algorithm = std::string(*param6);
 
-  String::Utf8Value param7(args[6]->ToString());
+  Nan::Utf8String param7(Nan::To<v8::String>(args[6]).ToLocalChecked());
   std::string pluginName = std::string(*param7);
 
   bool result = me->createPipeline(codec,width,height,framerateFPS,bitrateKbps,
@@ -104,7 +104,8 @@ void VideoGstAnalyzerWrap::linkInput(const FunctionCallbackInfo<Value>& args){
 
   unsigned int index = 0;
 
-  FrameSource* param8 = ObjectWrap::Unwrap<FrameSource>(args[0]->ToObject(Nan::GetCurrentContext()).ToLocalChecked());
+  FrameSource* param8 = ObjectWrap::Unwrap<FrameSource>(
+    Nan::To<v8::Object>(args[0]).ToLocalChecked());
   owt_base::FrameSource* src = param8->src;
 
   bool result = me->linkInput(src);
@@ -117,7 +118,8 @@ void VideoGstAnalyzerWrap::removeOutput(const FunctionCallbackInfo<Value>& args)
   VideoGstAnalyzerWrap* obj = ObjectWrap::Unwrap<VideoGstAnalyzerWrap>(args.Holder());
   mcu::VideoGstAnalyzer* me = obj->me;
 
-  FrameDestination* param8 = ObjectWrap::Unwrap<FrameDestination>(args[0]->ToObject());
+  FrameDestination* param8 = ObjectWrap::Unwrap<FrameDestination>(
+    Nan::To<v8::Object>(args[0]).ToLocalChecked());
   owt_base::FrameDestination* out = param8->dest;
 
   me->removeOutput(out);
@@ -129,7 +131,8 @@ void VideoGstAnalyzerWrap::addOutput(const FunctionCallbackInfo<Value>& args){
   VideoGstAnalyzerWrap* obj = ObjectWrap::Unwrap<VideoGstAnalyzerWrap>(args.Holder());
   mcu::VideoGstAnalyzer* me = obj->me;
 
-  FrameDestination* param8 = ObjectWrap::Unwrap<FrameDestination>(args[0]->ToObject(Nan::GetCurrentContext()).ToLocalChecked());
+  FrameDestination* param8 = ObjectWrap::Unwrap<FrameDestination>(
+    Nan::To<v8::Object>(args[0]).ToLocalChecked());
   owt_base::FrameDestination* out = param8->dest;
 
   bool result = me->addOutput(out);
@@ -141,12 +144,13 @@ void VideoGstAnalyzerWrap::addEventListener(const FunctionCallbackInfo<Value>& a
     Isolate* isolate = Isolate::GetCurrent();
     HandleScope scope(isolate);
     if (args.Length() < 2 || !args[0]->IsString() || !args[1]->IsFunction()) {
-        isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments")));
+        Nan::ThrowError("Wrong arguments");
         return;
     }
     VideoGstAnalyzerWrap* obj = ObjectWrap::Unwrap<VideoGstAnalyzerWrap>(args.Holder());
     if (!obj->me)
         return;
-    Local<Object>::New(isolate, obj->m_store)->Set(args[0], args[1]);
+    Nan::Set(Local<Object>::New(isolate, obj->m_store),
+             args[0], args[1]);
 }
 
