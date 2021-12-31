cbb05cb18ede4c3adff11d86b694c10ba1133715
Chen Li1 <li1.chen@intel.com>
2021-12-31 10:55:44 +0800

    Add management API for general stream service

--- source/management_api/resource/v1.1/index.js
+++ source/management_api/resource/v1.1/index.js
@@ -5,11 +5,15 @@
  * Router for v1.1 interfaces
  */
 'use strict';
-var express = require('express')
-var router = express.Router()
-var streamsResource = require('./streamsResource');
+const express = require('express')
+const router = express.Router()
+const streamsResource = require('./streamsResource');
+const tokensResource = require('../v1/tokensResource');
+const publicationsResource = require('./publicationsResource');
+const subscriptionsResource = require('./subscriptionsResource');
+const routerV1 = require('../v1');
 
-//Stream(including external streaming-in) management
+// Stream(including external streaming-in) management
 router.get('/rooms/:room/streams', streamsResource.getList);
 router.get('/rooms/:room/streams/:stream', streamsResource.get);
 router.patch('/rooms/:room/streams/:stream', streamsResource.patch);
@@ -17,4 +21,24 @@ router.delete('/rooms/:room/streams/:stream', streamsResource.delete);
 router.post('/rooms/:room/streaming-ins', streamsResource.addStreamingIn);//FIXME: Validation on body.type === 'streaming' is needed.
 router.delete('/rooms/:room/streaming-ins/:stream', streamsResource.delete);
 
-module.exports = router
+// Cluster
+router.post('/cluster/tokens', tokensResource.create);
+
+router.get('/cluster/publications', publicationsResource.getList);
+router.get('/cluster/publications/:publication', publicationsResource.get);
+router.post('/cluster/publications', publicationsResource.add);
+router.patch('/cluster/publications/:publication', publicationsResource.patch);
+router.delete('/cluster/publications/:publication', publicationsResource.delete);
+
+router.get('/cluster/subscriptions', subscriptionsResource.getList);
+router.get('/cluster/subscriptions/:subscription', subscriptionsResource.get);
+router.post('/cluster/subscriptions', subscriptionsResource.add);
+router.patch('/cluster/subscriptions/:subscription', subscriptionsResource.patch);
+router.delete('/cluster/subscriptions/:subscription', subscriptionsResource.delete);
+
+router.get('/cluster/nodes', publicationsResource.getNodes);
+
+// Same as previous version
+router.use(routerV1);
+
+module.exports = router;
---
+++ source/management_api/resource/v1.1/subscriptionsResource.js
@@ -0,0 +1,81 @@
+// Copyright (C) <2021> Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict';
+const requestHandler = require('../../requestHandler');
+const e = require('../../errors');
+
+// Logger
+const log = require('../../logger').logger.getLogger('SubscriptionsResource');
+
+const rpc = require('../../rpc/rpc');
+
+const STREAM_SERVICE_ID = 'stream-service';
+
+function callStreamService(methodName, args, callback) {
+  rpc.callRpc(STREAM_SERVICE_ID, methodName, args, {callback: function(ret) {
+    if (ret === 'timeout' || ret === 'error') {
+      callback(ret);
+    } else {
+      callback(null, ret);
+    }
+  }});
+}
+
+exports.getList = function (req, res, next) {
+  log.debug('Representing subscriptions for domain ', req.params.domain,
+            'and service', req.authData.service._id);
+  callStreamService('getSubscriptions', [{}], (err, pubs) => {
+    if (err) {
+      next(new e.CloudError('Failed to get subscriptions'));
+    } else {
+      res.send(pubs);
+    }
+  });
+};
+
+exports.get = function (req, res, next) {
+  log.debug('Representing subscription:', req.params.subscription,
+            ' for domain ', req.params.domain);
+  const query = {id: req.params.subscription};
+  callStreamService('getSubscriptions', [query], (err, pubs) => {
+    if (err) {
+      next(new e.CloudError('Failed to get subscriptions'));
+    } else {
+      res.send(pubs[0]);
+    }
+  });
+};
+
+exports.add = function (req, res, next) {
+  log.debug('Add subscription for domain ', req.params.domain);
+  callStreamService('subscribe', [req.body], (err, ret) => {
+    if (err) {
+      log.debug('Add failure:', err, err.stack);
+      next(new e.CloudError('Failed to subscribe'));
+    } else {
+      res.send(ret);
+    }
+  });
+};
+
+exports.patch = function (req, res, next) {
+  log.debug('Update subscription:', req.params.subscription,
+            ' for domain ', req.params.domain);
+  const stream = req.params.stream;
+  const cmds = req.body;
+  next(new e.CloudError('No implementation'));
+};
+
+exports.delete = function (req, res, next) {
+  log.debug('Delete subscription:', req.params.subscription,
+            ' for domain ', req.params.domain);
+  callStreamService('unsubscribe', [req.params.subscription], (err, ret) => {
+    if (err) {
+      next(new e.CloudError('Failed to unsubscribe'));
+    } else {
+      res.send(ret);
+    }
+  });
+};
