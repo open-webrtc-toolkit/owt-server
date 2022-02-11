59e2a0cdd4d6b3f0406cc1071d44a1e51e9d0e02
Tiger Meng <xiao.xi.meng@intel.com>
2022-02-11 19:44:35 +0800

    upgrade openssl 1.1.1m

--- scripts/installCommonDeps.sh
+++ scripts/installCommonDeps.sh
@@ -211,7 +211,7 @@ install_openssl(){
 
   if [ -d $LIB_DIR ]; then
     local SSL_BASE_VERSION="1.1.1"
-    local SSL_VERSION="1.1.1j"
+    local SSL_VERSION="1.1.1m"
     cd $LIB_DIR
     rm -f ./build/lib/libssl.*
     rm -f ./build/lib/libcrypto.*
