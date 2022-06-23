b347ee7e4dd4d57be0ccd1eb3f554a92af9abdf8
Chen Li1 <li1.chen@intel.com>
2022-06-23 10:07:07 +0800

    Enable GRPC option for analytics/quic agent

---
+++ source/agent/quic/grpcAdapter.js
@@ -0,0 +1,117 @@
+// Copyright (C) <2022> Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict';
+
+const unpackOption = require('./grpcTools').unpackOption;
+
+// Create GRPC interface for quic agent
+function createGrpcInterface(controller, streamingEmitter) {
+  const that = {};
+
+  // GRPC export
+  that.grpcInterface = {
+    publish: function (call, callback) {
+      const req = call.request;
+      const option = unpackOption(req.type, req.option);
+      controller.publish(req.id, req.type, option, (n, code, data) => {
+        if (code === 'error') {
+          callback(new Error(data), null);
+        } else {
+          callback(null, {id: req.id});
+        }
+      });
+    },
+    unpublish: function (call, callback) {
+      controller.unpublish(call.request.id, (n, code, data) => {
+        if (code === 'error') {
+          callback(new Error(data), null);
+        } else {
+          callback(null, {});
+        }
+      });
+    },
+    subscribe: function (call, callback) {
+      const req = call.request;
+      const option = unpackOption(req.type, req.option);
+      controller.subscribe(req.id, req.type, option, (n, code, data) => {
+        if (code === 'error') {
+          callback(new Error(data), null);
+        } else {
+          callback(null, {id: req.id});
+        }
+      });
+    },
+    unsubscribe: function (call, callback) {
+      controller.unsubscribe(call.request.id, (n, code, data) => {
+        if (code === 'error') {
+          callback(new Error(data), null);
+        } else {
+          callback(null, {});
+        }
+      });
+    },
+    linkup: function (call, callback) {
+      const req = call.request;
+      controller.linkup(
+        req.id, req.from,
+        (n, code, data) => {
+          if (code === 'error') {
+            callback(new Error(data), null);
+          } else {
+            callback(null, {message: data});
+          }
+        });
+    },
+    cutoff: function (call, callback) {
+      controller.cutoff(call.request.id, (n, code, data) => {
+        if (code === 'error') {
+          callback(new Error(data), null);
+        } else {
+          callback(null, {});
+        }
+      });
+    },
+    listenToNotifications: function (call, callback) {
+      streamingEmitter.on('notification', (notification) => {
+        const progress = {
+          type: 'quic',
+          name: notification.name,
+          data: notification.data,
+        };
+        call.write(progress);
+      });
+      streamingEmitter.on('close', () => {
+        call.end();
+      });
+    },
+    getInternalAddress: function (call, callback) {
+      controller.getInternalAddress((n, code, data) => {
+          if (code === 'error') {
+            callback(new Error(data), null);
+          } else {
+            callback(null, code);
+          }
+        });
+    },
+    validateTokenCallback: function (call, callback) {
+      call.on('data', (result) => {
+        streamingEmitter.emit('validateResult', result);
+      });
+      call.on('end', () => {
+        call.end();
+      });
+      streamingEmitter.on('token', (token) => {
+        call.write(token);
+      });
+      streamingEmitter.on('close', () => {
+        call.end();
+      });
+    }
+  };
+
+  return that;
+}
+
+exports.createGrpcInterface = createGrpcInterface;
