572006dbf286d62296c5f079b739f6140e62aee3
Chen Li1 <li1.chen@intel.com>
2022-07-21 15:17:02 +0800

    Enable GRPC option for conference agent

--- source/agent/conference/rpcRequest.js
+++ source/agent/conference/rpcRequest.js
@@ -9,75 +9,117 @@ const packOption = grpcTools.packOption;
 const unpackNotification = grpcTools.unpackNotification;
 
 const makeRPC = require('./makeRPC').makeRPC;
+const log = require('./logger').logger.getLogger('RpcRequest');
 
-const grpcPurposes = ['webrtc', 'audio', 'video', 'streaming', 'recording', 'analytics', 'quic'];
+const enableGrpc = global.config.agent.enable_grpc || false;
 
 const RpcRequest = function(rpcChannel, listener) {
   const that = {};
   const grpcAgents = {}; // workerAgent => grpcClient
   const grpcNode = {}; // workerNode => grpcClient
   const nodeType = {}; // NodeId => Type
+  let clusterClient;
 
   that.getRoomConfig = function(configServer, sessionId) {
     return rpcChannel.makeRPC(configServer, 'getRoomConfig', sessionId);
   };
 
   that.getWorkerNode = function(clusterManager, purpose, forWhom, preference) {
-    return rpcChannel.makeRPC(clusterManager, 'schedule', [purpose, forWhom.task, preference, 30 * 1000])
-      .then(function(workerAgent) {
-        if (grpcPurposes.includes(purpose)) {
-          if (!grpcAgents[workerAgent.id]) {
-            grpcAgents[workerAgent.id] = grpcTools.startClient(
-              'nodeManager',
-              workerAgent.id
-            );
+    if (enableGrpc) {
+      if (!clusterClient) {
+        clusterClient = grpcTools.startClient(
+          'clusterManager',
+          clusterManager
+        );
+      }
+      let agentAddress;
+      return new Promise((resolve, reject) => {
+        const req = {
+          purpose,
+          task: forWhom.task,
+          preference, // Change data for some preference
+          reserveTime: 30 * 1000
+        };
+        clusterClient.schedule(req, (err, result) => {
+          if (err) {
+            log.debug('Schedule node error:', err);
+            reject(err);
+          } else {
+            resolve(result);
           }
-          return new Promise((resolve, reject) => {
-            grpcAgents[workerAgent.id].getNode({info: forWhom}, (err, result) => {
-              if (!err) {
-                resolve(result.message);
-              } else {
-                reject(err);
-              }
-            });
-          }).then((workerNode) => {
-            // console.log('start client:', purpose, workerNode);
-            if (grpcNode[workerNode]) {
-              // Has client already
-              return {agent: workerAgent.id, node: workerNode};
+        });
+      }).then((workerAgent) => {
+        agentAddress = workerAgent.info.ip + ':' + workerAgent.info.grpcPort;
+        if (!grpcAgents[agentAddress]) {
+          grpcAgents[agentAddress] = grpcTools.startClient(
+            'nodeManager',
+            agentAddress
+          );
+        }
+        return new Promise((resolve, reject) => {
+          grpcAgents[agentAddress].getNode({info: forWhom}, (err, result) => {
+            if (!err) {
+              resolve(result.message);
+            } else {
+              reject(err);
             }
-            grpcNode[workerNode] = grpcTools.startClient(
-              purpose,
-              workerNode
-            );
-            nodeType[workerNode] = purpose;
-            // Register listener
-            const call = grpcNode[workerNode].listenToNotifications({id: ''});
-            call.on('data', (notification) => {
-              if (listener) {
-                // Unpack notification.data
-                const data = unpackNotification(notification);
-                if (data) {
-                  listener.processNotification(data);
-                }
-              }
-            });
-            call.on('end', () => {});
-            if (purpose === 'quic') {
-              // Register validate callback
-              const call = grpcNode[workerNode].validateTokenCallback();
-              call.on('data', (token) => {
-                // Validate token
-                listener.processCallback(token, (result) => {
-                  call.write({tokenId: token.tokenId, ok: result});
-                });
-              });
-              call.on('end', () => {});
+          });
+        });
+      }).then((workerNode) => {
+        if (grpcNode[workerNode]) {
+          // Has client already
+          return {agent: agentAddress, node: workerNode};
+        }
+        log.debug('Start gRPC client:', purpose, workerNode);
+        grpcNode[workerNode] = grpcTools.startClient(
+          purpose,
+          workerNode
+        );
+        nodeType[workerNode] = purpose;
+        // Register listener
+        const call = grpcNode[workerNode].listenToNotifications({id: ''});
+        call.on('data', (notification) => {
+          if (listener) {
+            // Unpack notification.data
+            const data = unpackNotification(notification);
+            if (data) {
+              listener.processNotification(data);
             }
-            return {agent: workerAgent.id, node: workerNode};
+          }
+        });
+        call.on('end', (err) => {
+          log.debug('Call on end:', err);
+          if (grpcNode[workerNode]) {
+            grpcNode[workerNode].close();
+            delete grpcNode[workerNode];
+          }
+        });
+        call.on('error', (err) => {
+          // On error
+          log.debug('Call on error:', err);
+        });
+        if (purpose === 'quic') {
+          // Register validate callback
+          const call = grpcNode[workerNode].validateTokenCallback();
+          call.on('data', (token) => {
+            // Validate token
+            listener.processCallback(token, (result) => {
+              call.write({tokenId: token.tokenId, ok: result});
+            });
+          });
+          call.on('end', () => {
+          });
+          call.on('error', (err) => {
+            // On error
+            log.debug('Call on error:', err);
           });
         }
+        return {agent: agentAddress, node: workerNode};
+      });
+    }
 
+    return rpcChannel.makeRPC(clusterManager, 'schedule', [purpose, forWhom.task, preference, 30 * 1000])
+      .then(function(workerAgent) {
         return rpcChannel.makeRPC(workerAgent.id, 'getNode', [forWhom])
           .then(function(workerNode) {
             return {agent: workerAgent.id, node: workerNode};
@@ -91,7 +133,7 @@ const RpcRequest = function(rpcChannel, listener) {
       return new Promise((resolve, reject) => {
         grpcAgents[workerAgent].recycleNode(req, (err, result) => {
           if (err) {
-            // console.log('recycle node:', err);
+            log.debug('Recycle node error:', err);
             reject(err);
           }
           if (grpcNode[workerNode]) {
@@ -110,7 +152,6 @@ const RpcRequest = function(rpcChannel, listener) {
   that.initiate = function(accessNode, sessionId, sessionType, direction, Options) {
     if (grpcNode[accessNode]) {
       // Test GRPC
-      // console.log('GRPC initiate:', sessionId, sessionType);
       return new Promise((resolve, reject) => {
         if (direction === 'in') {
           const req = {
@@ -218,10 +259,16 @@ const RpcRequest = function(rpcChannel, listener) {
   };
 
   that.sendMsg = function(portal, participantId, event, data) {
+    if (enableGrpc) {
+      return Promise.resolve();
+    }
     return rpcChannel.makeRPC(portal, 'notify', [participantId, event, data]);
   };
 
   that.broadcast = function(portal, controller, excludeList, event, data) {
+    if (enableGrpc) {
+      return Promise.resolve();
+    }
     return rpcChannel.makeRPC(portal, 'broadcast', [controller, excludeList, event, data]);
   };
 
@@ -434,9 +481,12 @@ const RpcRequest = function(rpcChannel, listener) {
           }
         });
       } else {
-        console.log('Unknow rpc name!');
+        log.error('Unknow rpc name:', rpcName);
       }
     } else {
+      if (enableGrpc) {
+        log.warn('Unknown gRPC node:', remoteNode);
+      }
       return makeRPC(_, remoteNode, rpcName, parameters, onOk, onError);
     }
   };
