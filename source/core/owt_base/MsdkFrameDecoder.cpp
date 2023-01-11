a61a03b633be215d9ef6166acb8605155d83f01b
Chen Li1 <li1.chen@intel.com>
2023-01-11 09:28:26 +0800

    Simplify code

--- source/core/owt_base/MsdkFrameDecoder.cpp
+++ source/core/owt_base/MsdkFrameDecoder.cpp
@@ -455,10 +455,8 @@ void MsdkFrameDecoder::updateBitstream(const Frame& frame)
                 , newSize
                 );
 
-        uint8_t* tmpDataBuf = (uint8_t*) m_bitstream->Data;
         m_bitstream->Data = (mfxU8 *)realloc(m_bitstream->Data, newSize);
         if (m_bitstream->Data == nullptr) {
-            free(tmpDataBuf);
             m_bitstream.reset();
             ELOG_ERROR_T("OOM! Allocate size %d", newSize);
             return;
